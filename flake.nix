539b065cbaa612e81811efa8694693fdd3014f84
matthewcroughan <matt@croughan.sh>
2022-03-08 15:40:06 +0000

    format flake.nix with alejandra

--- flake.nix
+++ flake.nix
@@ -7,57 +7,60 @@
       flake = false;
     };
   };
-  outputs = { self, nixpkgs,  flake-compat, flake-compat-ci, }@inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-compat
+    , flake-compat-ci
+    ,
+    }
+    @ inputs:
     let
       # System types to support.
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-    in {
-      devShell = forAllSystems (system: self.devShells.${system}.default);
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor."${system}";
-
-          latexEnv = with pkgs; texlive.combine {
-            inherit
-              (texlive)
-              scheme-basic
-              latexmk
-              ;
-            };
-
-        in rec
-        {
-          default = pkgs.mkShell {
-            name = "dUSD";
-            buildInputs = [
-              latexEnv
-              pkgs.entr
-            ];
+      nixpkgsFor = forAllSystems ( system: import nixpkgs { inherit system; } );
+    in
+    {
+      devShell = forAllSystems ( system: self.devShells.${ system }.default );
+      devShells =
+        forAllSystems
+          (
+            system:
+            let
+              pkgs = nixpkgsFor."${ system }";
+              latexEnv = with pkgs; texlive.combine { inherit ( texlive ) scheme-basic latexmk; };
+            in
+            rec {
+              default =
+                pkgs.mkShell
+                  {
+                    name = "dUSD";
+                    buildInputs = [ latexEnv pkgs.entr ];
+                  };
+            }
+          );
+      apps =
+        forAllSystems
+          (
+            system:
+            let
+              pkgs = nixpkgsFor."${ system }";
+            in
+            {
+              feedback-loop = {
+                type = "app";
+                program = "${ pkgs.callPackage ./nix/apps/feedback-loop { } }/bin/feedback-loop";
+              };
+            }
+          );
+      ciNix =
+        flake-compat-ci.lib.recurseIntoFlakeWith
+          {
+            flake = self;
+            systems = [ "x86_64-linux" ];
           };
-        });
-
-      apps = forAllSystems (system:
-      let
-        pkgs = nixpkgsFor."${system}";
-      in
-      {
-        feedback-loop = {
-          type = "app";
-          program = "${pkgs.callPackage ./nix/apps/feedback-loop{}}/bin/feedback-loop";
-        };
-    });
-
-      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
-        flake = self;
-        systems = [ "x86_64-linux" ];
-      };
-  };
+    };
 }
