2b854147fdc05cb9ab9bb6dbdb9a804680d972eb
Geometer1729 <16kuhnb@gmail.com>
2022-03-10 10:57:59 -0500

    adafy and unadfy edge

--- tests/Apropos/Plutus/SingletonValue.hs
+++ tests/Apropos/Plutus/SingletonValue.hs
@@ -4,6 +4,8 @@ module Apropos.Plutus.SingletonValue (
 import Apropos
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Hedgehog (fromGroup)
+import Control.Monad ( replicateM )
+import Data.String (IsString(..))
 import Plutus.V1.Ledger.Api
   ( CurrencySymbol
   , TokenName
@@ -43,13 +45,13 @@ instance HasPermutationGenerator SingletonValueProp SingletonValue where
     [ Morphism
         { name = "MakeZero"
         , match = Not $ Var IsZero
-        , contract = clear >> addAll [IsZero, IsSmall]
+        , contract = clear >> addAll [IsZero, IsSmall] >> addIf IsAda IsAda
         , morphism = \(c,t,_) -> pure (c,t,0)
         }
     , Morphism
         { name = "MakeLarge"
         , match = Not $ Var IsLarge
-        , contract = clear >> addAll [IsLarge, IsPositive]
+        , contract = clear >> addAll [IsLarge, IsPositive] >> addIf IsAda IsAda
         , morphism = \(c,t,_) -> do
             i <- int (linear 11 (maxBound -1))
             pure (c,t,fromIntegral i)
@@ -57,7 +59,8 @@ instance HasPermutationGenerator SingletonValueProp SingletonValue where
     , Morphism
         { name = "MakeSmall"
         , match = Not $ Var IsSmall
-        , contract = clear >> addAll [IsSmall, IsPositive]
+        -- TODO filtered clear might be nicer than addIf
+        , contract = clear >> addAll [IsSmall, IsPositive] >> addIf IsAda IsAda
         , morphism = \(c,t,_) -> do
             i <- int (linear 1 10)
             pure (c,t,fromIntegral i)
@@ -72,7 +75,31 @@ instance HasPermutationGenerator SingletonValueProp SingletonValue where
               ]
         , morphism = \(c,t,i) -> pure (c,t,-i)
         }
+    , Morphism
+      { name = "Adafy"
+      , match = Not $ Var IsAda
+      , contract = add IsAda
+      -- TODO once other currencies have props this will need to remove them
+      -- if we're going to need to distinguish amount properties
+      -- from asset properties should we just model this as
+      -- a pair of an assetClasses and an integer?
+      , morphism = \(_,_,i) -> pure ("","",i)
+      }
+    , Morphism
+      { name = "UnAdafy"
+      , match = Var IsAda
+      , contract = remove IsAda
+      , morphism = \(_,_,i) -> do
+          cs <- nonAdaName
+          tn <- nonAdaName
+          pure (cs,tn,i)
+      }
     ]
+
+nonAdaName :: IsString s => Gen s
+nonAdaName = fromString <$> replicateM 64 hexit
+  where
+    hexit = element $ ['0'..'9'] ++ ['a'..'f']
 
 instance HasParameterisedGenerator SingletonValueProp SingletonValue where
   parameterisedGenerator = buildGen baseGen
