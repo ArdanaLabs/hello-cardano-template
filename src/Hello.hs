f166d76e9e29b14206bed5c6ff7cb539fc577407
Geometer1729 <16kuhnb@gmail.com>
2022-04-01 14:18:43 -0400

    add serialize bytestring

--- src/Hello.hs
+++ src/Hello.hs
@@ -1,4 +1,4 @@
-module Hello (helloScript) where
+module Hello (helloScriptBytes, helloScript) where
 
 import Plutarch (ClosedTerm, compile)
 import Plutarch.Prelude
@@ -9,7 +9,12 @@ import Plutarch.Api.V1 (PMaybeData (PDJust), PScriptContext)
 import Plutarch.Builtin (pforgetData)
 
 import ApiUtils (findDatum, getContinuingOutputs)
+import Codec.Serialise (serialise)
+import Data.ByteString.Lazy (ByteString)
 import MonadUtils (tlet, tletField, tmatch, tmatchField)
+
+helloScriptBytes :: ByteString
+helloScriptBytes = serialise helloScript
 
 helloScript :: Script
 helloScript = compile validator
@@ -18,11 +23,11 @@ validator :: ClosedTerm (PInteger :--> PUnit :--> PScriptContext :--> PUnit)
 validator = plam $ \n _unit sc -> unTermCont $ do
   txinfo <- tletField @"txInfo" sc
   outs <- tlet $ getContinuingOutputs # sc
-  (PCons out mustBeEmpty) <- tmatch outs
+  PCons out mustBeEmpty <- tmatch outs
   PNil <- tmatch mustBeEmpty
-  (PDJust datumHash') <- tmatchField @"datumHash" out
+  PDJust datumHash' <- tmatchField @"datumHash" out
   datumHash <- tletField @"_0" datumHash'
-  (PJust datum) <- tmatch $ findDatum # datumHash # txinfo
+  PJust datum <- tmatch $ findDatum # datumHash # txinfo
   datumAsData <- tlet $ pforgetData $ pdata datum
   pure $
     pif
