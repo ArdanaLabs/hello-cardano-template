9868379ba3f04af04203fc845934dc601951706b
John Bargman <darthpjb@gmail.com>
2022-04-05 16:27:30 +0100

    corrected script to avoid lint error

--- nix/apps/feedback-loop/default.nix
+++ nix/apps/feedback-loop/default.nix
@@ -6,6 +6,6 @@ writeShellApplication
     name = "feedback-loop";
     runtimeInputs = [ entr ];
     text = ''
-      ls docs/*.tex | entr nix build .#build-docs
+      find docs -name "*.tex" | entr nix build .#build-docs
     '';
   }
