b86594f3f85cbe58c79d55676a9e1edffd75a365
Fraser Dunlop <fraser@mlabs.city>
2022-04-11 14:09:23 +0100

    flake: add onchain and offchain devShells

--- flake.nix
+++ flake.nix
@@ -37,69 +37,143 @@
           inherit (haskell-nix) config;
         };
 
-      projectFor = system: let
-        deferPluginErrors = true;
-        pkgs = nixpkgsFor system;
+      onchain = rec {
+        projectFor = system: let
+          deferPluginErrors = true;
+          pkgs = nixpkgsFor system;
 
-        fakeSrc = pkgs.runCommand "real-source" { } ''
-          cp -rT ${self} $out
-          chmod u+w $out/cabal.project
-          cat $out/cabal-haskell.nix.project >> $out/cabal.project
-        '';
-      in
-        (nixpkgsFor system).haskell-nix.cabalProject' {
-          src = fakeSrc.outPath;
-          compiler-nix-name = "ghc8107";
-          cabalProjectFileName = "cabal.project";
-          modules = [
-            {
-              packages = {
-                marlowe.flags.defer-plugin-errors = deferPluginErrors;
-                plutus-use-cases.flags.defer-plugin-errors = deferPluginErrors;
-                plutus-ledger.flags.defer-plugin-errors = deferPluginErrors;
-                plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
-                cardano-crypto-praos.components.library.pkgconfig =
-                  nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
-                cardano-crypto-class.components.library.pkgconfig =
-                  nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
+          fakeSrc = pkgs.runCommand "real-source-onchain" { } ''
+            cp -rT ${self}/onchain $out
+            chmod u+w $out/cabal.project
+            cat $out/cabal-haskell.nix.project >> $out/cabal.project
+          '';
+          in
+            (nixpkgsFor system).haskell-nix.cabalProject' {
+              src = fakeSrc.outPath;
+              compiler-nix-name = "ghc8107";
+              cabalProjectFileName = "cabal.project";
+              modules = [
+                {
+                  packages = {
+                    marlowe.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-use-cases.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-ledger.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
+                    cardano-crypto-praos.components.library.pkgconfig =
+                      nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
+                    cardano-crypto-class.components.library.pkgconfig =
+                      nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
+                  };
+                }
+              ];
+              shell = {
+                withHoogle = true;
+                tools = {
+                  haskell-language-server = { };
+                };
+
+                exactDeps = true;
+
+                # We use the ones from Nixpkgs, since they are cached reliably.
+                # Eventually we will probably want to build these with haskell.nix.
+                nativeBuildInputs = [ pkgs.cabal-install pkgs.hlint pkgs.haskellPackages.fourmolu ];
+
+                additional = ps: [
+                  ps.apropos
+                  ps.apropos-tx
+                  ps.plutarch
+                  ps.sydtest
+                  ps.sydtest-hedgehog
+                ];
               };
-            }
-          ];
-          shell = {
-            withHoogle = true;
-            tools = {
-              haskell-language-server = { };
+              sha256map = {
+                "https://github.com/mlabs-haskell/apropos"."455b1a3ad1eee35de4d3fb8c4a4527071474336c" = "sha256-EC6vnimXA+jBRPQLLs2dltuTx9XoSdkQfh742NnLJSQ=";
+                "https://github.com/mlabs-haskell/apropos-tx"."489eeb8c30d62d5c75eafe4242a1f133695f8564" = "sha256-15nFGPhXBy+G0oocb6KQf5KVnT0fuAOoFCdzT+vyeEg=";
+                "https://github.com/Plutonomicon/plutarch"."4052b285eb890799332c0cbe19cb08c1070f267a" = "sha256-8Tbrd9nPUkZarQiUTWYnNwbuz8wRhu+ipRK4XyedjTs=";
+                "https://github.com/input-output-hk/plutus.git"."983e6af2154c4bdf86ed645062bcb62f304d0a4f" = "sha256-Ga+hIhrgq2kR5Vnso/Edo2wgQpFn167eWGl35oM093U=";
+                "https://github.com/Quid2/flat.git"."ee59880f47ab835dbd73bea0847dab7869fc20d8" = "lRFND+ZnZvAph6ZYkr9wl9VAx41pb3uSFP8Wc7idP9M=";
+                "https://github.com/input-output-hk/cardano-crypto.git"."07397f0e50da97eaa0575d93bee7ac4b2b2576ec" = "oxIOVlgm07FAEmgGRF1C2me9TXqVxQulEOcJ22zpTRs=";
+                "https://github.com/input-output-hk/cardano-base"."78b3928391b558fb1750228f63301ec371f13528" = "pBUTTcenaSLMovHKGsaddJ7Jh3okRTrtu5W7Rdu6RM4=";
+                "https://github.com/input-output-hk/cardano-prelude"."fd773f7a58412131512b9f694ab95653ac430852" = "BtbT5UxOAADvQD4qTPNrGfnjQNgbYNO4EAJwH2ZsTQo=";
+                "https://github.com/input-output-hk/Win32-network"."3825d3abf75f83f406c1f7161883c438dac7277d" = "Hesb5GXSx0IwKSIi42ofisVELcQNX6lwHcoZcbaDiqc=";
+                "https://github.com/Srid/sydtest"."9c6c7678f7aabe22e075aab810a6a2e304591d24" = "sha256-P6ZwwfFeN8fpi3fziz9yERTn7BfxdE/j/OofUu+4GdA=";
+                "https://github.com/Srid/autodocodec"."42b42a7407f33c6c74fa4e8c84906aebfed28daf" = "sha256-X1TNZlmO2qDFk3OL4Z1v/gzvd3ouoACAiMweutsYek4=";
+                "https://github.com/Srid/validity"."f7982549b95d0ab727950dc876ca06b1862135ba" = "sha256-dpMIu08qXMzy8Kilk/2VWpuwIsfqFtpg/3mkwt5pdjA=";
+              };
             };
-
-            exactDeps = true;
-
-            # We use the ones from Nixpkgs, since they are cached reliably.
-            # Eventually we will probably want to build these with haskell.nix.
-            nativeBuildInputs = [ pkgs.cabal-install pkgs.hlint pkgs.haskellPackages.fourmolu ];
-
-            additional = ps: [
-              ps.apropos
-              ps.apropos-tx
-              ps.plutarch
-              ps.sydtest
-              ps.sydtest-hedgehog
-            ];
-          };
-          sha256map = {
-            "https://github.com/mlabs-haskell/apropos"."455b1a3ad1eee35de4d3fb8c4a4527071474336c" = "sha256-EC6vnimXA+jBRPQLLs2dltuTx9XoSdkQfh742NnLJSQ=";
-            "https://github.com/mlabs-haskell/apropos-tx"."489eeb8c30d62d5c75eafe4242a1f133695f8564" = "sha256-15nFGPhXBy+G0oocb6KQf5KVnT0fuAOoFCdzT+vyeEg=";
-            "https://github.com/Plutonomicon/plutarch"."4052b285eb890799332c0cbe19cb08c1070f267a" = "sha256-8Tbrd9nPUkZarQiUTWYnNwbuz8wRhu+ipRK4XyedjTs=";
-            "https://github.com/input-output-hk/plutus.git"."983e6af2154c4bdf86ed645062bcb62f304d0a4f" = "sha256-Ga+hIhrgq2kR5Vnso/Edo2wgQpFn167eWGl35oM093U=";
-            "https://github.com/Quid2/flat.git"."ee59880f47ab835dbd73bea0847dab7869fc20d8" = "lRFND+ZnZvAph6ZYkr9wl9VAx41pb3uSFP8Wc7idP9M=";
-            "https://github.com/input-output-hk/cardano-crypto.git"."07397f0e50da97eaa0575d93bee7ac4b2b2576ec" = "oxIOVlgm07FAEmgGRF1C2me9TXqVxQulEOcJ22zpTRs=";
-            "https://github.com/input-output-hk/cardano-base"."78b3928391b558fb1750228f63301ec371f13528" = "pBUTTcenaSLMovHKGsaddJ7Jh3okRTrtu5W7Rdu6RM4=";
-            "https://github.com/input-output-hk/cardano-prelude"."fd773f7a58412131512b9f694ab95653ac430852" = "BtbT5UxOAADvQD4qTPNrGfnjQNgbYNO4EAJwH2ZsTQo=";
-            "https://github.com/input-output-hk/Win32-network"."3825d3abf75f83f406c1f7161883c438dac7277d" = "Hesb5GXSx0IwKSIi42ofisVELcQNX6lwHcoZcbaDiqc=";
-            "https://github.com/Srid/sydtest"."9c6c7678f7aabe22e075aab810a6a2e304591d24" = "sha256-P6ZwwfFeN8fpi3fziz9yERTn7BfxdE/j/OofUu+4GdA=";
-            "https://github.com/Srid/autodocodec"."42b42a7407f33c6c74fa4e8c84906aebfed28daf" = "sha256-X1TNZlmO2qDFk3OL4Z1v/gzvd3ouoACAiMweutsYek4=";
-            "https://github.com/Srid/validity"."f7982549b95d0ab727950dc876ca06b1862135ba" = "sha256-dpMIu08qXMzy8Kilk/2VWpuwIsfqFtpg/3mkwt5pdjA=";
-          };
         };
+    in let
+      onchain-scripts = system: (nixpkgsFor system).stdenv.mkDerivation {
+        name = "onchain-scripts";
+        src = self;
+        buildInputs = [ ((onchain.projectFor system).flake {}).packages."dUSD-onchain:exe:scripts" ];
+        doCheck = false;
+        installPhase = ''
+          scripts "$out"
+          '';
+        configurePhase = ''
+          mkdir $out
+              '';
+      };
+
+      offchain = rec {
+        projectFor = system: let
+          deferPluginErrors = true;
+          pkgs = nixpkgsFor system;
+
+          fakeSrc = pkgs.runCommand "real-source-offchain" { } ''
+            cp -rT ${self}/offchain $out
+            chmod u+w $out/cabal.project
+            cat $out/cabal-haskell.nix.project >> $out/cabal.project
+          '';
+          in
+            (nixpkgsFor system).haskell-nix.cabalProject' {
+              src = fakeSrc.outPath;
+              compiler-nix-name = "ghc8107";
+              cabalProjectFileName = "cabal.project";
+              modules = [
+                {
+                  packages = {
+                    marlowe.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-use-cases.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-ledger.flags.defer-plugin-errors = deferPluginErrors;
+                    plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
+                    cardano-crypto-praos.components.library.pkgconfig =
+                      nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
+                    cardano-crypto-class.components.library.pkgconfig =
+                      nixpkgs.lib.mkForce [ [ (import plutus { inherit system; }).pkgs.libsodium-vrf ] ];
+                  };
+                }
+              ];
+              shell = {
+                withHoogle = true;
+                tools = {
+                  haskell-language-server = { };
+                };
+
+                exactDeps = true;
+
+                # We use the ones from Nixpkgs, since they are cached reliably.
+                # Eventually we will probably want to build these with haskell.nix.
+                nativeBuildInputs = [ pkgs.cabal-install pkgs.hlint pkgs.haskellPackages.fourmolu ];
+
+                additional = ps: [
+                  ps.plutarch
+                ];
+                DUSD_SCRIPTS = onchain-scripts system;
+              };
+              sha256map = {
+                "https://github.com/Plutonomicon/plutarch"."4052b285eb890799332c0cbe19cb08c1070f267a" = "sha256-8Tbrd9nPUkZarQiUTWYnNwbuz8wRhu+ipRK4XyedjTs=";
+                "https://github.com/input-output-hk/plutus.git"."983e6af2154c4bdf86ed645062bcb62f304d0a4f" = "sha256-Ga+hIhrgq2kR5Vnso/Edo2wgQpFn167eWGl35oM093U=";
+                "https://github.com/Quid2/flat.git"."ee59880f47ab835dbd73bea0847dab7869fc20d8" = "lRFND+ZnZvAph6ZYkr9wl9VAx41pb3uSFP8Wc7idP9M=";
+                "https://github.com/input-output-hk/cardano-crypto.git"."07397f0e50da97eaa0575d93bee7ac4b2b2576ec" = "oxIOVlgm07FAEmgGRF1C2me9TXqVxQulEOcJ22zpTRs=";
+                "https://github.com/input-output-hk/cardano-base"."78b3928391b558fb1750228f63301ec371f13528" = "pBUTTcenaSLMovHKGsaddJ7Jh3okRTrtu5W7Rdu6RM4=";
+                "https://github.com/input-output-hk/cardano-prelude"."fd773f7a58412131512b9f694ab95653ac430852" = "BtbT5UxOAADvQD4qTPNrGfnjQNgbYNO4EAJwH2ZsTQo=";
+                "https://github.com/input-output-hk/Win32-network"."3825d3abf75f83f406c1f7161883c438dac7277d" = "Hesb5GXSx0IwKSIi42ofisVELcQNX6lwHcoZcbaDiqc=";
+              };
+            };
+        };
+
 
         lintSpec = {
           cabal-fmt = {};
@@ -109,32 +183,58 @@
           # Enable after https://github.com/ArdanaLabs/dUSD/issues/8
           # nixpkgs-fmt = {};
         };
+
+        # Checks the shell script using ShellCheck
+        checkedShellScript = system: name: text:
+          ((nixpkgsFor system).writeShellApplication {
+            inherit name text;
+          }) + "/bin/${name}";
+
+        # Take a flake app (identified as the key in the 'apps' set), and return a
+        # derivation that runs it in the compile phase.
+        #
+        # In effect, this allows us to run an 'app' as part of the build process (eg: in CI).
+        flakeApp2Derivation = system: appName:
+          (nixpkgsFor system).runCommand appName { } "${self.apps.${system}.${appName}.program} | tee $out";
     in
       {
-        project = forAllSystems projectFor;
-        flake = forAllSystems (system: (projectFor system).flake { });
+        onchain = {
+          project = forAllSystems onchain.projectFor;
+          flake = forAllSystems (system: (onchain.projectFor system).flake { });
+        };
+
+        offchain = {
+          project = forAllSystems offchain.projectFor;
+          flake = forAllSystems (system: (offchain.projectFor system).flake { });
+        };
+
 
         # this could be done automatically, but would reduce readability
         packages = forAllSystems (system:
-        let pkgs = (forAllSystems nixpkgsFor)."${system}";
-        in self.flake.${system}.packages // {
-          test-plan = pkgs.stdenv.mkDerivation {
-            name = "test-plan";
-            src = self;
-            buildInputs = with pkgs; [ (texlive.combine { inherit ( texlive ) scheme-basic latexmk todonotes metafont; }) ];
-            doCheck = false;
-            buildPhase = ''
-              HOME=$TMP latexmk -output-directory="$out" -pdf ./docs/test-plan.tex
-              ls -lah
-            '';
-            installPhase = ''
-              ls -lah
-            '';
-          };
+          let pkgs = (forAllSystems nixpkgsFor)."${system}";
+          in self.onchain.flake.${system}.packages
+          // self.offchain.flake.${system}.packages
+          // {
+            test-plan = pkgs.stdenv.mkDerivation {
+              name = "test-plan";
+              src = self;
+              buildInputs = with pkgs; [ (texlive.combine { inherit ( texlive ) scheme-basic latexmk todonotes metafont; }) ];
+              doCheck = false;
+              buildPhase = ''
+                HOME=$TMP latexmk -output-directory="$out" -pdf ./docs/test-plan.tex
+                ls -lah
+              '';
+              installPhase = ''
+                ls -lah
+              '';
+            };
         });
+
         checks = forAllSystems (system:
-          self.flake.${system}.checks //
-            (lint-utils.mkChecks.${system} lintSpec ./.)
+             self.onchain.flake.${system}.checks
+          // { offchain-test = flakeApp2Derivation system "offchain-test";
+             }
+          // (lint-utils.mkChecks.${system} lintSpec ./.)
         );
         # We need this attribute because `nix flake check` won't work for Haskell
         # projects: https://nixos.wiki/wiki/Import_From_Derivation#IFD_and_Haskell
@@ -143,12 +243,25 @@
         check = forAllSystems (
           system:
             (nixpkgsFor system).runCommand "combined-test" {
-              nativeBuildInputs = builtins.attrValues self.checks.${system};
-            } "touch $out"
+              checksss = builtins.attrValues self.checks.${system}
+              ++ [
+                self.devShells.${system}.onchain.inputDerivation
+                self.devShells.${system}.offchain.inputDerivation
+              ];
+            } ''
+              echo $checksss
+              touch $out
+              ''
         );
 
-        devShell = forAllSystems (system: self.flake.${system}.devShell);
-        defaultPackage = forAllSystems (system: self.packages.${system}."dUSD:test:tests");
+        devShells = forAllSystems (system: {
+          onchain = self.onchain.flake.${system}.devShell;
+          offchain = self.offchain.flake.${system}.devShell;
+        });
+        defaultPackage = forAllSystems (system:
+             self.packages.${system}."dUSD-onchain:test:tests"
+          // self.packages.${system}."dUSD-offchain:exe:tests"
+        );
         apps = forAllSystems (system: let
           pkgs = (forAllSystems nixpkgsFor)."${system}";
         in
@@ -158,6 +271,14 @@
               program = "${ pkgs.callPackage ./nix/apps/feedback-loop { } }/bin/feedback-loop";
             };
             format =  lint-utils.mkApp.${system} lintSpec;
+            offchain-test = {
+              type = "app";
+              program = checkedShellScript system "dUSD-offchain-test"
+                '' export DUSD_SCRIPTS=${onchain-scripts system}
+                   cd ${self}
+                   ${self.packages.${system}."dUSD-offchain:exe:tests"}/bin/tests;
+                '';
+            };
           });
       };
 }
