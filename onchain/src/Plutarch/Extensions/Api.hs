1ccc8498a06711a397d20ea9c7afc095f35fad8e
Sridhar Ratnakumar <srid@srid.ca>
2022-04-19 12:57:51 -0400

    Use p*C naming, to be consistent with plutarch-extra

--- onchain/src/Plutarch/Extensions/Api.hs
+++ onchain/src/Plutarch/Extensions/Api.hs
@@ -1,6 +1,6 @@
 module Plutarch.Extensions.Api (
-  getContinuingDatum,
-  passert,
+  pgetContinuingDatumC,
+  passertC,
 ) where
 
 import Plutarch.Prelude
@@ -19,9 +19,9 @@ import Plutarch.TryFrom (PTryFrom)
 {- | enfroces that there is a unique continuing output gets it's Datum
  - and converts it to the desired type via pfromData
 -}
-getContinuingDatum :: forall p s. (PTryFrom PData (PAsData p)) => Term s PScriptContext -> TermCont s (Term s (PAsData p))
-getContinuingDatum sc = do
-  ctxF <- tcont $ pletFields @["txInfo", "purpose"] sc
+pgetContinuingDatumC :: forall p s. (PTryFrom PData (PAsData p)) => Term s PScriptContext -> TermCont s (Term s (PAsData p))
+pgetContinuingDatumC ctx = do
+  ctxF <- tcont $ pletFields @["txInfo", "purpose"] ctx
   pmatchC (getField @"purpose" ctxF) >>= \case
     PSpending outRef -> do
       txInfoF <- tcont $ pletFields @["inputs", "outputs", "datums"] $ getField @"txInfo" ctxF
@@ -42,5 +42,5 @@ getContinuingDatum sc = do
       pure perror
 
 -- | fails with provided message if the bool is false otherwise returns unit
-passert :: Term s PString -> Term s PBool -> Term s PUnit
-passert msg bool = pif bool (pcon PUnit) (ptraceError msg)
+passertC :: Term s PString -> Term s PBool -> TermCont s (Term s PUnit)
+passertC msg bool = pure $ pif bool (pcon PUnit) (ptraceError msg)
