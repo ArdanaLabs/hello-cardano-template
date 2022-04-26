41b705f15d164a6e5005f5a828706cef13d49eb8
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-04-26 17:01:35 +0200

    offchain/hello-world: add initialize endpoint impl

--- offchain/hello-world/src/HelloWorld/Contract.hs
+++ offchain/hello-world/src/HelloWorld/Contract.hs
@@ -4,23 +4,56 @@ module HelloWorld.Contract (
   contract,
 ) where
 
+import Codec.Serialise (deserialise)
 import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString.Lazy qualified as BSL
+import Data.Map qualified as Map
 import Data.Text (Text)
+import System.Environment (getEnv)
+
+import Ledger.Constraints (adjustUnbalancedTx, otherScript, mustPayToTheScript)
+import Ledger.Tx (getCardanoTxId)
+import Ledger.Value (Value(..))
+import Ledger.Ada (adaValueOf)
+import Ledger.Typed.Scripts (ValidatorTypes(..))
+import Plutus.V2.Ledger.Api (Validator(..))
 import Plutus.Contract (
+  AsContractError,
   Contract,
   Endpoint,
   type (.\/),
+  mkTxConstraints,
+  submitUnbalancedTx,
+  awaitTxConfirmed,
+  logInfo
  )
 import Plutus.Contract qualified as PContract
 
+data Hello
+instance ValidatorTypes Hello where
+  type instance RedeemerType Hello = ()
+  type instance DatumType Hello = Integer
+
+getHelloValidator :: IO Validator
+getHelloValidator = do
+  Validator . deserialise <$> (getEnv "DUSD_SCRIPTS" >>= BSL.readFile . (++ "/hello_world.plc"))
+
 -- | REST schema
-type Schema =
-  Endpoint "initialize" ()
+type HelloWorldSchema =
+        Endpoint "initialize" Integer
     .\/ Endpoint "increment" ()
     .\/ Endpoint "read" ()
 
-initialize :: () -> Contract w s e ()
-initialize _ = pure ()
+initialize :: AsContractError e => Integer -> Contract w s e ()
+initialize initialInt = do
+  helloValidator <- liftIO getHelloValidator
+  let lookups = otherScript helloValidator
+      tx = mustPayToTheScript initialInt (adaValueOf 0)
+  adjustedTx <- adjustUnbalancedTx <$> mkTxConstraints @Hello lookups tx
+  ledgerTx <- submitUnbalancedTx adjustedTx
+  awaitTxConfirmed $ getCardanoTxId ledgerTx
+  logInfo @String $ "Successfully initialized datum with value: " <> show initialInt
 
 increment :: () -> Contract w s e ()
 increment _ = pure ()
@@ -29,7 +62,7 @@ readContract :: () -> Contract w s e ()
 readContract _ = pure ()
 
 -- | The contract definition
-contract :: Contract () Schema Text ()
+contract :: Contract () HelloWorldSchema Text ()
 contract =
   forever $
     PContract.selectList
