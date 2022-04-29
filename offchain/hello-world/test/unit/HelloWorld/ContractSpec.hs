c6d92ff968ae018111d8195363d40d57fb91147f
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-04-29 11:16:27 +0200

    offchain/hello-world: add datum assertion to test

--- offchain/hello-world/test/unit/HelloWorld/ContractSpec.hs
+++ offchain/hello-world/test/unit/HelloWorld/ContractSpec.hs
@@ -1,24 +1,46 @@
-module HelloWorld.ContractSpec (testTree, insertValidDataTrace) where
+module HelloWorld.ContractSpec (testTree, initializeHelloWorldDatum, incrementHelloWorldDatum, readHelloWorldDatum) where
 
 import Data.Functor (void)
 import Test.Tasty
 
 import Plutus.Contract.Test
+import Plutus.V1.Ledger.Api (fromBuiltinData)
 import Plutus.Trace.Emulator
 
+import HelloWorld.ValidatorProxy (helloValidatorAddress)
 import HelloWorld.Contract (contract)
 
 testTree :: TestTree
 testTree = testGroup "HelloWorld Contract Emulator Traces" [
-    checkPredicate "updating valid data should not fail" (assertNoFailedTransactions (insertValidDataTrace)
+    checkPredicate "should initialize correctly" (assertNoFailedTransactions .&&. assertHelloWorldDatumEquals 1) initializeHelloWorldDatum
+  , checkPredicate "should increment once" (assertNoFailedTransactions .&&. assertHelloWorldDatumEquals 2) incrementHelloWorldDatum
   ]
 
-insertValidDataTrace :: EmulatorTrace ()
-insertValidDataTrace = do
+assertHelloWorldDatumEquals :: Integer -> TracePredicate
+assertHelloWorldDatumEquals expected = dataAtAddress helloValidatorAddress $ \ ds ->
+  any ((Just expected ==) . fromBuiltinData) ds
+
+initializeHelloWorldDatum :: EmulatorTrace ()
+initializeHelloWorldDatum = do
+  h1 <- activateContractWallet (knownWallet 1) contract
+  void $ waitNSlots 1
+  callEndpoint @"initialize" h1 1
+  void $ waitNSlots 1
+
+incrementHelloWorldDatum :: EmulatorTrace ()
+incrementHelloWorldDatum = do
   h1 <- activateContractWallet (knownWallet 1) contract
   void $ waitNSlots 1
   callEndpoint @"initialize" h1 1
   void $ waitNSlots 1
   callEndpoint @"increment" h1 ()
   void $ waitNSlots 1
-  return ()
+
+readHelloWorldDatum :: EmulatorTrace ()
+readHelloWorldDatum = do
+  h1 <- activateContractWallet (knownWallet 1) contract
+  void $ waitNSlots 1
+  callEndpoint @"initialize" h1 1
+  void $ waitNSlots 1
+  callEndpoint @"increment" h1 ()
+  void $ waitNSlots 1
