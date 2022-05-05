b9d2a533f6579333e42e55bc4c46ac07be31b37a
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-05-05 11:34:39 +0200

    ada-price-feeder: update exp

--- price-feeder/ada-price-feeder/src/MyLib.hs
+++ price-feeder/ada-price-feeder/src/MyLib.hs
@@ -9,8 +9,11 @@ import Statistics.Quantile (median, medianUnbiased)
 import Servant.Client (BaseUrl, ClientEnv, mkClientEnv)
 import Network.HTTP.Client.TLS (newTlsManager)
 
+import Network.Binance.Client (binanceBaseUrl, getPrice)
 import Network.Coinbase.Client (coinbaseBaseUrl, getSpotPrice)
+import Network.Huobi.Client (huobiBaseUrl, getTick)
 import Network.Kraken.Client (krakenBaseUrl, getOHLCData)
+import Network.Kucoin.Client (kucoinBaseUrl, getFiatPrice)
 import PriceData.Types (CurrencyPair(..))
 
 adaUsd :: CurrencyPair
@@ -19,14 +22,35 @@ adaUsd = CurrencyPair {
   , _currency = "USD"
   }
 
+adaUsdt :: CurrencyPair
+adaUsdt = CurrencyPair {
+    _base = "ADA"
+  , _currency = "USDT"
+  }
+
+getBinancePrice :: (BaseUrl -> ClientEnv) -> IO (Either T.Text Double)
+getBinancePrice env = do
+  _ <- getPrice (env binanceBaseUrl) adaUsdt
+  undefined
+
 getCoinbasePrice :: (BaseUrl -> ClientEnv) -> IO (Either T.Text Double)
 getCoinbasePrice env = do
   _ <- getSpotPrice (env coinbaseBaseUrl) adaUsd Nothing
   undefined
 
+getHuobiPrice :: (BaseUrl -> ClientEnv) -> IO (Either T.Text Double)
+getHuobiPrice env = do
+  _ <- getTick (env huobiBaseUrl) adaUsdt
+  undefined
+
 getKrakenPrice :: (BaseUrl -> ClientEnv) -> IO (Either T.Text Double)
 getKrakenPrice env = do
   _ <- getOHLCData (env krakenBaseUrl) adaUsd Nothing Nothing
+  undefined
+
+getKucoinPrice :: (BaseUrl -> ClientEnv) -> IO (Either T.Text Double)
+getKucoinPrice env = do
+  _ <- getFiatPrice (env kucoinBaseUrl) adaUsd
   undefined
 
 -- see catMaybes
@@ -36,7 +60,7 @@ getMedianPriceFromSources = do
   manager <- newTlsManager
   let clientEnv = mkClientEnv manager
       fetchers :: [IO (Either T.Text Double)]
-      fetchers = ($ clientEnv) <$> [getCoinbasePrice, getKrakenPrice]
+      fetchers = ($ clientEnv) <$> [getBinancePrice, getCoinbasePrice, getHuobiPrice, getKrakenPrice, getKucoinPrice]
   results <- (fmap $ fromRight 1) <$> sequenceA fetchers
   return $ median medianUnbiased $ V.fromList results
 
