df0148c96378f25a14df77b295b8a6855be0a9e1
Péter Diviánszky <divipp@gmail.com>
2022-05-12 16:32:40 +0200

    add missing hs file

---
+++ offchain/hello-world/selenium-example/app/Main.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
+module Main where
+
+import Test.WebDriver
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Text (unpack)
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Paths_try_selenium
+
+data Command = Init | Inc | Read
+  deriving Show
+
+instance Arbitrary Command where
+  arbitrary = elements [Init, Inc, Inc, Inc, Inc, Read, Read]
+
+evalCommands :: [Command] -> String
+evalCommands = snd . foldl (flip f) (0, "")  where
+
+  f Init (_, c) = (0, c)
+  f Inc (i, c) = (i + 1, c)
+  f read (i, _) = (i, show i)
+
+firefoxConfig = defaultConfig
+-- chromeConfig = useBrowser chrome defaultConfig
+
+prop :: [Command] -> Property
+prop commands = monadicIO $ do
+
+  path <- liftIO getDataDir
+  n <- run $ runSession firefoxConfig . finallyClose $ do
+
+    openPage $ "file://" <> path <> "/HelloWorld.html"
+
+    initialize <- findElem $ ById "initialize"
+    increment  <- findElem $ ById "increment"
+    read       <- findElem $ ById "read"
+    counter    <- findElem $ ById "counterr"
+
+    let interpret c = click $ case c of
+         Init -> initialize
+         Inc -> increment
+         Read -> read
+
+    mapM_ interpret commands
+    unpack <$> getText counter
+
+  assert $ n == evalCommands commands
+
+main :: IO ()
+main = quickCheck $ mapSize (*10) $ withMaxSuccess 10 prop
+
+
