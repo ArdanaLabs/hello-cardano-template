b4d56c0aa2702265d2508776d1b0f77b3739c438
Sridhar Ratnakumar <srid@srid.ca>
2022-05-12 11:03:23 -0400

    nix: Run nixpkgs-fmt on flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     plutus.url = "github:input-output-hk/plutus";
     plutus-apps.url = "github:input-output-hk/plutus-apps?rev=e4062bca213f233cdf9822833b07aa69dff6d22a";
   };
-  outputs = {self, nixpkgs, haskell-nix, flake-utils, ...}@inputs:
+  outputs = { self, nixpkgs, haskell-nix, flake-utils, ... }@inputs:
     let
       # Function that produces Flake outputs for the given system.
       #
@@ -22,98 +22,98 @@
       # We use flake-utils.lib.eachSystem (see below) to call this.
       # cf. https://github.com/NixOS/nix/issues/3843#issuecomment-661720562
       outputsFor = system:
-    # NOTE: The body of this function is de-indented twice to keep diff clean.
-    # We should use nixpkgs-fmt later in a single commit.
-    let
-      # TODO: We probably should use a non-haskell.nix nixpkgs for certain
-      # derivations, to speed up things. Those derivations do not rely on
-      # haskell.nix anyway. Consider this in the context of passing `pkgs` to
-      # sub-flake'ishes.
-      pkgs = 
-        import nixpkgs {
-          inherit system;
-          overlays = [ haskell-nix.overlay ];
-          inherit (haskell-nix) config;
-        };
-      projects = {
-        onchain = import ./onchain { inherit inputs system pkgs; };
-        offchain = import ./offchain { inherit inputs system pkgs; };
-        docs = import ./docs { inherit inputs system pkgs; };
-        format = import ./nix/format.nix { inherit inputs system pkgs; };
-      };
-
-    in
-      {
-        inherit projects;
-
-        # TODO: Refactor this composition so it takes a projects set, without
-        # needing to look inside.
-        packages =
-             projects.onchain.packages
-          // projects.offchain.packages
-          // projects.docs.packages
-          // {
-            # A combination of all derivations (aside from packages) we are care
-            # to build in CI: checks, devShells. We need this because IFD in
-            # Haskell disallows use of `nix flake check`, but we can use `nix
-            # build .#ci` to, effectively, run those checks.
-            ci = pkgs.runCommand "combined" {
-              drvs = 
-                builtins.attrValues self.checks.${system}
-                # This allows us to cache nix-shell (nix develop)
-                # https://nixos.wiki/wiki/Caching_nix_shell_build_inputs
-                ++ (pkgs.lib.attrsets.mapAttrsToList 
-                      (_: shell: shell.inputDerivation) 
-                      self.devShells.${system});
-            } ''echo $drvs > $out'';
+        let
+          # TODO: We probably should use a non-haskell.nix nixpkgs for certain
+          # derivations, to speed up things. Those derivations do not rely on
+          # haskell.nix anyway. Consider this in the context of passing `pkgs` to
+          # sub-flake'ishes.
+          pkgs =
+            import nixpkgs {
+              inherit system;
+              overlays = [ haskell-nix.overlay ];
+              inherit (haskell-nix) config;
+            };
+          projects = {
+            onchain = import ./onchain { inherit inputs system pkgs; };
+            offchain = import ./offchain { inherit inputs system pkgs; };
+            docs = import ./docs { inherit inputs system pkgs; };
+            format = import ./nix/format.nix { inherit inputs system pkgs; };
           };
-        defaultPackage = self.packages.${system}.ci;
 
-        checks =
-             projects.onchain.checks
-          // projects.offchain.checks
-          // projects.format.checks;
+        in
+        {
+          inherit projects;
 
-        # In Nix, there is no builtin way to access the project root, where
-        # flake.nix lives. To workaround this, we inject it as env var in the
-        # `shellHook`.
-        flakeRoot = {
-          shellHook = ''
-            export FLAKE_ROOT=$(pwd)
-          '';
-          envVar = "$FLAKE_ROOT";
+          # TODO: Refactor this composition so it takes a projects set, without
+          # needing to look inside.
+          packages =
+            projects.onchain.packages
+            // projects.offchain.packages
+            // projects.docs.packages
+            // {
+              # A combination of all derivations (aside from packages) we are care
+              # to build in CI: checks, devShells. We need this because IFD in
+              # Haskell disallows use of `nix flake check`, but we can use `nix
+              # build .#ci` to, effectively, run those checks.
+              ci = pkgs.runCommand "combined"
+                {
+                  drvs =
+                    builtins.attrValues self.checks.${system}
+                    # This allows us to cache nix-shell (nix develop)
+                    # https://nixos.wiki/wiki/Caching_nix_shell_build_inputs
+                    ++ (pkgs.lib.attrsets.mapAttrsToList
+                      (_: shell: shell.inputDerivation)
+                      self.devShells.${system});
+                } ''echo $drvs > $out'';
+            };
+          defaultPackage = self.packages.${system}.ci;
+
+          checks =
+            projects.onchain.checks
+            // projects.offchain.checks
+            // projects.format.checks;
+
+          # In Nix, there is no builtin way to access the project root, where
+          # flake.nix lives. To workaround this, we inject it as env var in the
+          # `shellHook`.
+          flakeRoot = {
+            shellHook = ''
+              export FLAKE_ROOT=$(pwd)
+            '';
+            envVar = "$FLAKE_ROOT";
+          };
+
+          # We are forced to use two devshells.
+          # Under ideal circumstances, we could put all the onchain and offchain
+          # code in the same project, sharing the same cabal.project, but this is
+          # not possible because:
+          #
+          # On-chain code requires recent versions of plutarch, which uses a
+          # more recent version of `plutus` than is in `plutus-apps`.
+          #
+          # So, in order to remove this hack and use one cabal project instead, we need:
+          #
+          # Plutarch to be more or less stable so that it can use the version
+          # of `plutus` that is in `plutus-apps` at the time, instead of a recent
+          # one.
+          #
+          # There was also the idea of using a plutus-tx (so not Plutarch)
+          # dummy-implementation of an on-chain validator until these two
+          # conditions are met. We opted not to do this because it would require
+          # us to bet that the condition above would be met before we want to launch.
+          devShells = {
+            onchain = projects.onchain.devShell;
+            offchain = projects.offchain.devShell;
+          };
+
+          apps =
+            projects.offchain.apps
+            // projects.docs.apps
+            // projects.format.apps;
         };
-
-        # We are forced to use two devshells.
-        # Under ideal circumstances, we could put all the onchain and offchain
-        # code in the same project, sharing the same cabal.project, but this is
-        # not possible because:
-        #
-        # On-chain code requires recent versions of plutarch, which uses a
-        # more recent version of `plutus` than is in `plutus-apps`.
-        #
-        # So, in order to remove this hack and use one cabal project instead, we need:
-        #
-        # Plutarch to be more or less stable so that it can use the version
-        # of `plutus` that is in `plutus-apps` at the time, instead of a recent
-        # one.
-        #
-        # There was also the idea of using a plutus-tx (so not Plutarch)
-        # dummy-implementation of an on-chain validator until these two
-        # conditions are met. We opted not to do this because it would require
-        # us to bet that the condition above would be met before we want to launch.
-        devShells = {
-          onchain = projects.onchain.devShell;
-          offchain = projects.offchain.devShell;
-        };
-
-        apps =
-             projects.offchain.apps
-          // projects.docs.apps
-          // projects.format.apps;
-      };
-  in flake-utils.lib.eachSystem [ "x86_64-linux" ] outputsFor // {
-    # Name of our project; used in script prefixes.
-    projectName = "dusd";
-  };
+    in
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] outputsFor // {
+      # Name of our project; used in script prefixes.
+      projectName = "dusd";
+    };
 }
