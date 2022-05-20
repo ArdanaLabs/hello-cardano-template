495fb8e3034832e15861d2508ed0412e35a65d01
Mason Mackaman <masondeanm@aol.com>
2022-05-20 14:33:10 -0400

    Restructure purs-nix usage

--- offchain/hello-world-ui/flake-module.nix
+++ offchain/hello-world-ui/flake-module.nix
@@ -1,46 +1,42 @@
 { self, ... }:
 {
   perSystem = system: { config, self', inputs', ... }:
-  let
-    make-purs-nix = system:
-      let
-        purs-nix = self.inputs.purs-nix { inherit system; };
-        inherit (purs-nix) ps-pkgs purs;
-      in
-      purs-nix
-      // purs
-           { dependencies =
-               with ps-pkgs;
-               [ aff
-                 affjax
-                 affjax-web
-                 argonaut-codecs
-                 argonaut-core
-                 effect
-                 either
-                 foreign-object
-                 halogen
-                 http-methods
-                 maybe
-                 prelude
-                 remotedata
-                 transformers
-                 tuples
-                 type-equality
-               ];
-             srcs = [ ./src ];
-           };
+    let
       pkgs = inputs'.nixpkgs.legacyPackages;
       projectName = "hello-world-ui";
-      inherit (make-purs-nix system) modules;
-      purs-nix = make-purs-nix system;
+      purs-nix = self.inputs.purs-nix { inherit system; };
+
+      ps =
+        purs-nix.purs
+          { dependencies =
+              with purs-nix.ps-pkgs;
+              [ aff
+                affjax
+                affjax-web
+                argonaut-codecs
+                argonaut-core
+                effect
+                either
+                foreign-object
+                halogen
+                http-methods
+                maybe
+                prelude
+                remotedata
+                transformers
+                tuples
+                type-equality
+              ];
+
+            srcs = [ ./src ];
+          };
     in
     {
       packages = {
         "${projectName}" = pkgs.runCommand projectName {}
           ''
           mkdir $out; cd $out
-          ln -s ${modules.Main.bundle { esbuild.minify = true; }} main.js
+          ln -s ${ps.modules.Main.bundle { esbuild.minify = false; }} main.js
           ln -s ${./dist/index.html} index.html
           '';
       };
@@ -62,11 +58,11 @@
         inputsFrom = builtins.attrValues self'.packages;
         buildInputs = (with pkgs; [
           nodejs-16_x
-          (purs-nix.command {})
+          (ps.command {})
           purs-nix.esbuild
           purs-nix.purescript
           purs-nix.purescript-language-server
-          purs-nix.purs-tidy
+          # purs-nix.purs-tidy
         ]);
       };
     };
