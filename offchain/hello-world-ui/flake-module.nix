c886f818d0609af1c3dee53cba3d83525b8fb64f
Hao <hao.liu@platonic.systems>
2022-05-23 23:21:06 +0000

    Revert "Generate output with 'purs-nix compile' so language server will be happy"
    This reverts commit 44c3660ce17ff4c9fd89018c0d29ef5bd94ac31e.

--- offchain/hello-world-ui/flake-module.nix
+++ offchain/hello-world-ui/flake-module.nix
@@ -71,17 +71,6 @@
           purs-nix.purescript-language-server
           # purs-nix.purs-tidy
         ]);
-        shellHook = ''
-          if [ -d ".git" ]
-          then
-            cd offchain/hello-world-ui
-          fi
-
-          if [ ! -d "output" ]
-          then
-              purs-nix compile
-          fi
-        '';
       };
     };
   flake = {
