7a9a6926a0afaa715eb247d50e9ce8b76b17c982
Geometer1729 <16kuhnb@gmail.com>
2022-06-08 11:17:06 -0400

    seperate hello-ctl to fix ci and gitignore .envrc

---
+++ onchain/hello-ctl/HelloWorld.hs
@@ -0,0 +1,32 @@
+module Main (main) where
+
+import Codec.Serialise (serialise)
+import Data.ByteString.Lazy qualified as BSL
+import Data.Char (chr)
+import Data.Word (Word8)
+import Hello (helloValidator)
+import Plutus.V1.Ledger.Scripts (Validator)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  [out] <- getArgs
+  writeFile (out ++ "/hello_world.cbor") hellWorldHexString
+
+
+hellWorldHexString :: String
+hellWorldHexString = validatorToHexString helloValidator
+
+validatorToHexString :: Validator -> String
+validatorToHexString v = concatMap byteToHex $ BSL.unpack $ serialise v
+  where
+    byteToHex :: Word8 -> String
+    byteToHex b =
+      let h1 = b `div` 16
+          h2 = b `mod` 16
+       in [toChr h1, toChr h2]
+
+    toChr :: Word8 -> Char
+    toChr w
+      | w < 10 = chr $ fromIntegral $ w + 48
+      | otherwise = chr $ fromIntegral $ w + 87
--- onchain/scripts/Main.hs
+++ onchain/scripts/Main.hs
@@ -2,8 +2,6 @@ module Main (main) where
 
 import Codec.Serialise (serialise)
 import Data.ByteString.Lazy qualified as BSL
-import Data.Char (chr)
-import Data.Word (Word8)
 import Hello (helloValidator)
 import Plutus.V1.Ledger.Scripts (getValidator)
 import System.Environment (getArgs)
@@ -11,16 +9,4 @@ import System.Environment (getArgs)
 main :: IO ()
 main = do
   [out] <- getArgs
-  let bsl = serialise $ getValidator helloValidator
-  writeFile (out ++ "/hello_world.plc") (concatMap byteToHex $ BSL.unpack bsl)
-
-byteToHex :: Word8 -> String
-byteToHex b =
-  let h1 = b `div` 16
-      h2 = b `mod` 16
-   in [toChr h1, toChr h2]
-
-toChr :: Word8 -> Char
-toChr w
-  | w < 10 = chr $ fromIntegral $ w + 48
-  | otherwise = chr $ fromIntegral $ w + 87
+  BSL.writeFile (out ++ "/hello_world.plc") (serialise (getValidator helloValidator))
