11066fc1935d5eab8feac581fda92741e1bd45d0
Geometer1729 <16kuhnb@gmail.com>
2022-06-08 12:41:58 -0400

    better error handleing

--- onchain/hello-world/HelloWorld.hs
+++ onchain/hello-world/HelloWorld.hs
@@ -1,9 +1,18 @@
 module Main (main) where
 
 import Hello (helloWorldHexString)
+
+import Control.Monad (unless)
+import System.Directory (doesDirectoryExist)
 import System.Environment (getArgs)
+import System.Exit (die)
 
 main :: IO ()
 main = do
-  [out] <- getArgs
-  writeFile (out ++ "/hello_world.cbor.txt") helloWorldHexString
+  getArgs >>= \case
+    [out] -> do
+      exists <- doesDirectoryExist out
+      unless exists $ die $ "directory: " <> out <> " does not exist"
+      writeFile (out ++ "/hello_world.cbor.txt") helloWorldHexString
+    _ -> do
+      die "usage: cabal run hello-world <file_path>"
