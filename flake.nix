3723f18668265ac83bd32e74913e68ee1c8e6023
MatthewCroughan <matt@croughan.sh>
2022-06-09 02:19:08 +0100

    hello-world-ui: remove easy-purescript-nix dependency (#154)
    flake.lock: Update
    
    Flake lock file updates:
    
    • Removed input 'easy-purescript-nix'
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/1d7db1b9e4cf1ee075a9f52e5c36f7b9f4207502' (2022-05-16)
      → 'github:nixos/nixpkgs/033bd4fa9a8fbe0c68a88e925d9a884161044b25' (2022-06-07)

--- flake.nix
+++ flake.nix
@@ -23,10 +23,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     purs-nix.url = "github:ursi/purs-nix";
-    easy-purescript-nix = {
-      url = "github:justinwoo/easy-purescript-nix";
-      flake = false;
-    };
   };
 
   outputs = { self, flake-modules-core, ... }:
