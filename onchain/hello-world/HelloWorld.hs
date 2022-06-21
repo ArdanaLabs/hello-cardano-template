46acc06842b9a5c9758590216bc19000f0eb88b2
Geometer1729 <16kuhnb@gmail.com>
2022-06-21 17:00:20 -0400

    generate purscript instead of plain cbor

--- onchain/hello-world/HelloWorld.hs
+++ onchain/hello-world/HelloWorld.hs
@@ -6,10 +6,12 @@ import Control.Monad (unless)
 import System.Directory (doesDirectoryExist)
 import System.Environment (getArgs)
 import System.Exit (die)
+import Data.List (intercalate)
 
 {- | Main takes a directory as a comand line argument
- and puts the cbor hex string of the hello world validator
- into a file named `hello_world.cbor.txt` in that directory.
+  and creates a file CBOR.purs in that directory
+  which will provide variables as configured in
+  the cbors constant
 -}
 main :: IO ()
 main = do
@@ -17,10 +19,23 @@ main = do
     [out] -> do
       exists <- doesDirectoryExist out
       unless exists $ die $ "directory: " <> out <> " does not exist"
-      writeFile (out ++ "/hello_world.cbor.txt") paramHelloCBOR
+      writeFile (out ++ "/CBOR.purs") $ toPs cbors
     _ -> do
       die "usage: cabal run hello-world <file_path>"
 
--- TODO
--- we may end up with more executables very similar to this
--- if so we should make a helper function
+cbors :: [CBOR]
+cbors =
+  [ CBOR "paramHello" paramHelloCBOR
+  , CBOR "hello" helloWorldHexString
+  ]
+
+data CBOR = CBOR{name :: String,cbor :: String}
+
+toPs :: [CBOR] -> String
+toPs cs =
+  "module CBOR (\n  " <> intercalate ",\n  " (name <$> cs) <> "\n) where\n\n"
+  <> intercalate "\n\n" (toDec <$> cs)
+
+toDec :: CBOR -> String
+toDec c = name c <> " :: String\n" <> name c <> " = \""
+  <> cbor c <> "\""
