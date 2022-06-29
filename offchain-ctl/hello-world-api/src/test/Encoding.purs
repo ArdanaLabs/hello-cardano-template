04d054d4da1b64951d88fe01b9b89c0487ceeb51
Geometer1729 <16kuhnb@gmail.com>
2022-06-29 11:12:23 -0400

    requested changes for spec readability

--- offchain-ctl/hello-world-api/src/test/Encoding.purs
+++ offchain-ctl/hello-world-api/src/test/Encoding.purs
@@ -7,16 +7,21 @@ import Contract.Prelude
 
 import CBOR as CBOR
 import Test.Spec(Spec,it,describe)
-import Test.Spec.Assertions(shouldNotEqual)
+import Test.Spec.Assertions(shouldNotEqual,shouldEqual)
 import Contract.Aeson (decodeAeson, fromString)
 import Contract.Scripts (Validator)
 
 spec :: Spec Unit
-spec = do
-  describe "hello" $ testCborDecodes CBOR.hello
+spec = describe "decodeCbor" $ do
+  it "fails on invalid" $
+    decodeCbor "invalid" `shouldEqual` Nothing
+  it "hello decodes" $
+    decodeCbor CBOR.hello `shouldNotEqual` Nothing
 
-testCborDecodes :: String -> Spec Unit
-testCborDecodes cbor = it "decode test" $ decodeCbor cbor `shouldNotEqual` Nothing
+-- TODO shouldSatisify isJust would be better
+-- as it wouldn't needlesly require (Eq a)
+-- and imo is more readable
+-- but shouldSatisify fails to import for some reason
 
 decodeCbor :: String -> Maybe Validator
 decodeCbor = fromString >>> decodeAeson >>> hush >>> map wrap
--- offchain-ctl/hello-world-api/src/test/Main.purs
+++ offchain-ctl/hello-world-api/src/test/Main.purs
@@ -6,10 +6,9 @@ import Prelude
 import Effect (Effect)
 import Test.Encoding as Encoding
 import Effect.Aff (launchAff_)
-import Test.Spec(describe)
 import Test.Spec.Runner (runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
-  describe "Encoding" Encoding.spec
+  Encoding.spec
