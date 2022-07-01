daf783b41eab7388c253c9fa44f5c3a110c0dda5
Geometer1729 <16kuhnb@gmail.com>
2022-07-01 12:10:28 -0400

    add failing test

--- offchain-ctl/hello-world-api/src/test/Main.purs
+++ offchain-ctl/hello-world-api/src/test/Main.purs
@@ -6,9 +6,12 @@ import Prelude
 import Effect (Effect)
 import Test.Encoding as Encoding
 import Effect.Aff (launchAff_)
+import Test.Spec(it)
+import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Runner (runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   Encoding.spec
+  it "should fail" $ 0 `shouldEqual` 1
