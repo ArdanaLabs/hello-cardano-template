fa8cb2e515779109119ca438cb84ff552bd8573e
Geometer1729 <16kuhnb@gmail.com>
2022-07-06 10:38:16 -0400

    remove failing test

--- offchain-ctl/hello-world-api/src/test/Main.purs
+++ offchain-ctl/hello-world-api/src/test/Main.purs
@@ -9,10 +9,6 @@ import Effect.Aff (launchAff_)
 import Test.Spec.Runner (runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
 
-import Test.Spec(it)
-import Test.Spec.Assertions(shouldEqual)
-
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   Encoding.spec
-  it "should fail" $ 1 `shouldEqual` 0
