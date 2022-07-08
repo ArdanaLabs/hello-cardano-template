844d38d60b6555081ade39a428dfd1e1d161afcb
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-07-08 19:09:06 +0200

    price-feeder/kraken: remove comments

--- price-feeder/kraken-servant-api/test/Network/Kraken/APISpec.hs
+++ price-feeder/kraken-servant-api/test/Network/Kraken/APISpec.hs
@@ -71,15 +71,3 @@ testParseTickData = do
                                 ]
                           }
                    )
-
--- AssetTickerInfo {
---     _ask = PriceAndVolume 52609.60000 1 1.00
---   , _bid = PriceAndVolume 52609.50000 1 1.00
---   , _lastTradeClosed = (52641.10000, 0.00080000)
---   , _volume = (1920.83610601, 7954.00219674)
---   , _volumeWeightedAveragePrice = (52389.94668, 54022.90683)
---   , _numberOfTrades = (23329, 80463)
---   , _low = (51513.90000, 51513.90000)
---   , _high = (53219.90000, 57200.00000)
---   , _todaysOpeningPrice = 52280.40000
--- }
