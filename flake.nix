152d47d57b2c15c922815114282057ad077aa318
Geometer1729 <16kuhnb@gmail.com>
2022-07-12 14:29:07 -0400

    update purs-nix which had already fixed args thing

--- flake.nix
+++ flake.nix
@@ -29,8 +29,8 @@
       flake = false;
       url = "github:nix-community/npmlock2nix";
     };
-    purs-nix.url = "github:Geometer1729/purs-nix";
-    purs-nix-0-14.url = "github:Geometer1729/purs-nix/ps-0.14";
+    purs-nix.url = "github:ursi/purs-nix";
+    purs-nix-0-14.url = "github:ursi/purs-nix/ps-0.14";
   };
 
   outputs = { self, flake-modules-core, ... }:
