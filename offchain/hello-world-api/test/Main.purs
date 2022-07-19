76b9073551f6463ce43c0b73f669168045f7c7c0
Geometer1729 <16kuhnb@gmail.com>
2022-07-19 10:58:28 -0400

    disable timeouts so keywallet spec works

--- offchain/hello-world-api/test/Main.purs
+++ offchain/hello-world-api/test/Main.purs
@@ -8,10 +8,11 @@ import Test.Encoding as Encoding
 import KeyWallet as KeyWallet
 
 import Effect.Aff (launchAff_)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner (runSpec',defaultConfig)
 import Test.Spec.Reporter.Console (consoleReporter)
+import Data.Maybe(Maybe(Nothing))
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] do
+main = launchAff_ $ runSpec' defaultConfig{timeout=Nothing} [ consoleReporter ] do
   Encoding.spec
   KeyWallet.spec
