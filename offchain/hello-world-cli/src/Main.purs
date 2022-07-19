5601f22e9af6a7ad346e072894b20f9b33a5820f
Geometer1729 <16kuhnb@gmail.com>
2022-07-19 09:43:26 -0400

    fix typo

--- offchain/hello-world-cli/src/Main.purs
+++ offchain/hello-world-cli/src/Main.purs
@@ -6,7 +6,7 @@ import Contract.Prelude
 import Effect.Aff(launchAff_)
 import Options.Applicative(execParser)
 import HelloWorld.Cli.Parser(parser)
-import HelloWorld.Cli.Runners(runCLI)
+import HelloWorld.Cli.Runners(runCli)
 
 main :: Effect Unit
 main = launchAff_ $ runCli =<< liftEffect (execParser parser)
