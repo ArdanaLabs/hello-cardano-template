a83a68341ff5cba24a3605d0028ada5cdb7e61a2
Geometer1729 <16kuhnb@gmail.com>
2022-07-19 16:55:58 -0400

    first cli tests in purescript

--- offchain/hello-world-cli/test/Main.purs
+++ offchain/hello-world-cli/test/Main.purs
@@ -4,15 +4,24 @@ module Test.Main
 
 import Contract.Prelude
 import Effect.Aff(launchAff_)
-import CmdUtils(fails,passes)
-import Test.Spec(it)
+import CmdUtils(fails,passes,spawnAff)
+import Test.Spec(it,describe)
 import Test.Spec.Runner (runSpec',defaultConfig)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 main :: Effect Unit
-main = launchAff_ $
+main = launchAff_ $ do
+  _ <- spawnAff "nix build .#hello-world-cli"
+  let cli = "./result/bin/hello-world-cli "
   runSpec' defaultConfig{timeout=Nothing} [ consoleReporter ] do
-  it "ls fails on bad_path" $ fails "ls bad_path"
-  it "ls -a passes" $ passes "ls -a"
-  pure unit
+    describe "shell sanity checks" do
+      it "ls err"
+        $ fails "ls bad_path"
+      it "ls pass"
+        $ passes "ls -a"
+    describe "lock" do
+      it "fails no conf"
+        $ fails $ cli <> "-s state lock -i 0 -p 1"
+      it "fails no state"
+        $ fails $ cli <> "-c conf lock -i 0 -p 1"
 
