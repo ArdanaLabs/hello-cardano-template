32bf8404ec0cdc06cfc0f45e06be90a70a75f0fe
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-07-26 20:25:07 +0000

    refactor: refactor hello-world-browser packages in offchain flake module (#253)
    * refactor: refactor hello-world-browser packages in offchain flake module
    
    * style: ran format

--- offchain/flake-module.nix
+++ offchain/flake-module.nix
@@ -162,64 +162,55 @@
       prefixOutputs = dusd-lib.prefixAttrNames "offchain";
     in
     {
-      packages = prefixOutputs {
-        inherit hello-world-cbor;
-        hello-world-api = hello-world-api.package;
-        docs =
-          pkgs.runCommand "offchain-docs" { }
-            ''
-              mkdir $out && cd $out
-              # it may make sense to eventually add cli and browser to the srcs, but we need to not define Main twice
-              ${hello-world-api.ps.command { srcs = [ ./hello-world-api/src ];} }/bin/purs-nix docs
-            '';
-        hello-world-browser =
-          pkgs.runCommand "build-hello-world-browser" { }
-            # see buildPursProjcet: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L74
-            # see bundlePursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L149
-            ''
-              mkdir $out && cd $out
-              export BROWSER_RUNTIME=1
-              cp -r ${hello-world-browser.ps.modules.Main.output {}} output
-              cp ${./hello-world-browser/index.js} index.js
-              cp ${./hello-world-browser/index.html} index.html
-              cp ${./webpack.config.js} webpack.config.js
-              cp -r ${ctlNodeModules}/* .
-              export NODE_PATH="node_modules"
-              export PATH="bin:$PATH"
-              mkdir dist
-              cp ${./hello-world-browser/main.css} dist/main.css
-              webpack --mode=production -c webpack.config.js -o ./dist --entry ./index.js
-            '';
-        hello-world-browser-for-testing =
-          pkgs.runCommand "build-hello-world-browser-for-testing" { }
-            # see buildPursProjcet: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L74
-            # see bundlePursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L149
-            ''
-              mkdir $out && cd $out
-              export BROWSER_RUNTIME=1
-              cp -r ${hello-world-browser.ps.modules.Main.output {}} output
-              cp ${./hello-world-browser/test.js} index.js
-              cp ${./hello-world-browser/index.html} index.html
-              cp ${./webpack.config.js} webpack.config.js
-              cp -r ${ctlNodeModules}/* .
-              export NODE_PATH="node_modules"
-              export PATH="bin:$PATH"
-              mkdir dist
-              cp ${./hello-world-browser/main.css} dist/main.css
-              webpack --mode=production -c webpack.config.js -o ./dist --entry ./index.js
-            '';
-        hello-world-cli =
-          let js = "${hello-world-cli.ps.modules.Main.output {}}/Main/index.js"; in
-          pkgs.writeScriptBin "hello-world-cli"
-            ''
-              export NODE_PATH=${ctlNodeModules}/node_modules
-              ${pkgs.nodejs}/bin/node \
-                --preserve-symlinks \
-                --input-type=module \
-                -e 'import { main } from "${js}"; main()' \
-                -- "hello-world-cli" "''$@"
-            '';
-      };
+      packages =
+        let
+          make-hello-world-browser-package = { indexJs }:
+            pkgs.runCommand "build-hello-world-browser" { }
+              # see buildPursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L74
+              # see bundlePursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L149
+              ''
+                mkdir $out && cd $out
+                export BROWSER_RUNTIME=1
+                cp -r ${hello-world-browser.ps.modules.Main.output {}} output
+                cp ${indexJs} index.js
+                cp ${./hello-world-browser/index.html} index.html
+                cp ${./webpack.config.js} webpack.config.js
+                cp -r ${ctlNodeModules}/* .
+                export NODE_PATH="node_modules"
+                export PATH="bin:$PATH"
+                mkdir dist
+                cp ${./hello-world-browser/main.css} dist/main.css
+                webpack --mode=production -c webpack.config.js -o ./dist --entry ./index.js
+              '';
+        in
+        prefixOutputs {
+          inherit hello-world-cbor;
+          hello-world-api = hello-world-api.package;
+          docs =
+            pkgs.runCommand "offchain-docs" { }
+              ''
+                mkdir $out && cd $out
+                # it may make sense to eventually add cli and browser to the srcs, but we need to not define Main twice
+                ${hello-world-api.ps.command { srcs = [ ./hello-world-api/src ];} }/bin/purs-nix docs
+              '';
+          hello-world-browser =
+            make-hello-world-browser-package
+              { indexJs = ./hello-world-browser/index.js; };
+          hello-world-browser-for-testing =
+            make-hello-world-browser-package
+              { indexJs = ./hello-world-browser/test.js; };
+          hello-world-cli =
+            let js = "${hello-world-cli.ps.modules.Main.output {}}/Main/index.js"; in
+            pkgs.writeScriptBin "hello-world-cli"
+              ''
+                export NODE_PATH=${ctlNodeModules}/node_modules
+                ${pkgs.nodejs}/bin/node \
+                  --preserve-symlinks \
+                  --input-type=module \
+                  -e 'import { main } from "${js}"; main()' \
+                  -- "hello-world-cli" "''$@"
+              '';
+        };
 
       checks = {
         run-hello-world-api-tests =
