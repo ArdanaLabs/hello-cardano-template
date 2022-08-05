77d0eb38e3ad5332d0961c96292b2096b43e9490
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-05 11:07:25 +0200

    offchain/hello-world-api-tests: remove unused imports

--- offchain/hello-world-api/test/Main.purs
+++ offchain/hello-world-api/test/Main.purs
@@ -4,14 +4,13 @@ module Test.Main
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (logShow)
 import Test.Encoding as Encoding
+import Test.HelloWorld.Api as Test.HelloWorld.Api
 
 import Effect.Aff (launchAff_)
 import Test.Spec.Runner (runSpec',defaultConfig)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Data.Maybe(Maybe(Nothing),isNothing)
-import Test.HelloWorld.Api as Test.HelloWorld.Api
+import Data.Maybe(Maybe(Nothing))
 
 main :: Effect Unit
 main = do
