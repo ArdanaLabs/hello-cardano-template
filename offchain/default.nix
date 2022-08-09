324721b74100c23fadbf112646537a30038d70e9
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-08-09 17:01:55 +0300

    refactor(nix): reorganize offchain nix directory to be consistent with root nix directory

--- offchain/default.nix
+++ offchain/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./flake-module.nix
-    ./nix/default.nix
+    ./nix/flake-modules/default.nix
     ./hello-world-browser/flake-module.nix
     ./hello-world-cli/flake-module.nix
     ./hello-world-api/flake-module.nix
