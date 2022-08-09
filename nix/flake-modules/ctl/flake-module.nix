f7935ebf42e508286457dd859b358444152b80a1
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-08-09 17:08:03 +0300

    style(nix): format files

--- nix/flake-modules/ctl/flake-module.nix
+++ nix/flake-modules/ctl/flake-module.nix
@@ -7,20 +7,20 @@ let
 in
 {
   perSystem = { inputs', ... }:
-  let
-    pkgs = inputs'.nixpkgs.legacyPackages;
-    npmlock2nix = pkgs.callPackages self.inputs.npmlock2nix { };
-  in
-  {
-    options = {
-      ctl = {
-        nodeModules = mkOption {
-          type = types.package;
-          default =
-            npmlock2nix.node_modules
-              { src = self.inputs.cardano-transaction-lib; };
+    let
+      pkgs = inputs'.nixpkgs.legacyPackages;
+      npmlock2nix = pkgs.callPackages self.inputs.npmlock2nix { };
+    in
+    {
+      options = {
+        ctl = {
+          nodeModules = mkOption {
+            type = types.package;
+            default =
+              npmlock2nix.node_modules
+                { src = self.inputs.cardano-transaction-lib; };
+          };
         };
       };
     };
-  };
 }
