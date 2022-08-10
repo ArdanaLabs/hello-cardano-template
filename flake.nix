8a9211255668477aff96daff5244c07ae923f120
Geometer1729 <16kuhnb@gmail.com>
2022-08-10 15:45:55 -0400

    get started on plutip for cli tests

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cardano-node.url = "github:input-output-hk/cardano-node?rev=73f9a746362695dc2cb63ba757fbcabb81733d23";
-    cardano-transaction-lib.url = "github:Plutonomicon/cardano-transaction-lib?rev=c4f27bac079fae2ccc18599587afad6a8ef51971";
+    cardano-transaction-lib.url = "github:Plutonomicon/cardano-transaction-lib?rev=6cdbad6e3250101ac909c582be743c9ab2566d99";
     cardano-ogmios.url = "github:input-output-hk/cardano-ogmios";
     mlabs-ogmios.follows = "cardano-transaction-lib/ogmios";
     ogmios-datum-cache.follows = "cardano-transaction-lib/ogmios-datum-cache";
---
+++ offchain/hello-world-cli/test/Wallet.purs
@@ -0,0 +1,30 @@
+module Test.Wallet
+  (makeWallet
+  ) where
+
+import Contract.Prelude
+import Wallet.Key(KeyWallet)
+import Contract.Wallet.KeyFile(privatePaymentKeyToFile,privateStakeKeyToFile)
+import Wallet.Key(keyWalletPrivatePaymentKey,keyWalletPrivateStakeKey)
+import Node.FS.Aff
+  (readTextFile
+  ,writeTextFile
+  ,unlink
+  )
+import Node.Encoding (Encoding(UTF8))
+import Aeson (encodeAeson)
+
+makeWallet :: String -> String -> KeyWallet -> Aff Unit
+makeWallet dir name wallet = do
+  let cfgName = dir <> name <>  "-cfg.json"
+  let walletName = dir <> name <> "-wallet.skey"
+  let stakeName = dir <> name <> "-staking.skey"
+  writeTextFile UTF8 cfgName $ encodeAeson >>>show $
+    { walletPath : walletName
+    , stakingPath : stakeName
+      -- TODO this needs to handle nothing case
+      -- CLI also nees to handle nothing case
+    , network : "Testnet" -- TODO this is wrong
+    }
+  privatePaymentKeyToFile walletName $ keyWalletPrivatePaymentKey wallet
+  void $ traverse (privateStakeKeyToFile stakeName) $ keyWalletPrivateStakeKey wallet
