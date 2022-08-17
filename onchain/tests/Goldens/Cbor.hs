4cf75717c50481cb03423e20313835c8bd74f26b
Geometer1729 <16kuhnb@gmail.com>
2022-08-17 09:50:21 -0400

    clean up goldens

--- onchain/tests/Goldens/Cbor.hs
+++ onchain/tests/Goldens/Cbor.hs
@@ -1,13 +1,19 @@
 module Goldens.Cbor (spec) where
 
+import Data.List (intercalate)
+import Data.Text (unpack)
 import Hello (helloWorldCbor, paramHelloCbor, trivialCbor)
 import Test.Syd
 
 spec :: Spec
-spec = describe "goldens" $ do
-  it "trivial" $
-    pureGoldenStringFile "./goldens/trivial" (show trivialCbor)
-  it "hello" $
-    pureGoldenStringFile "./goldens/hello" helloWorldCbor
-  it "param-hello" $
-    pureGoldenStringFile "./goldens/param-hello" (show paramHelloCbor)
+spec = describe "cbor" $ do
+  defaultGolden "trivial" trivialCbor
+  defaultGolden "hello" helloWorldCbor
+  defaultGolden "param-hello" paramHelloCbor
+
+-- If we add more goldens this should probably be moved somewhere more central ie. a new Goldens.hs
+defaultGolden :: Show a => String -> a -> _
+defaultGolden name a = do
+  path <- reverse <$> getTestDescriptionPath
+  let path' = "./" <> intercalate "/" (unpack <$> path) <> "/" <> name
+  it name $ pureGoldenStringFile path' (show a)
