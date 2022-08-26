b6a75b1a57d7079024be0bd61facb8ac71b469b8
Geometer1729 <16kuhnb@gmail.com>
2022-08-26 14:00:08 -0400

    move hello to v2

--- onchain/src/Hello.hs
+++ onchain/src/Hello.hs
@@ -14,13 +14,16 @@ module Hello (
 import Data.Default (Default (def))
 import Utils (closedTermToHexString, validatorToHexString)
 
-import PlutusLedgerApi.V1.Address (Address (..))
-import PlutusLedgerApi.V1.Credential (Credential (..))
-import PlutusLedgerApi.V1.Scripts (Validator, ValidatorHash)
+import PlutusLedgerApi.V2
+  (Validator
+  ,ValidatorHash
+  ,Address(..)
+  ,Credential(..)
+  )
 
 import Plutarch.Prelude
 
-import Plutarch.Api.V1 (PScriptContext, PValidator, mkValidator, validatorHash)
+import Plutarch.Api.V2 (PScriptContext, PValidator, mkValidator, validatorHash)
 import Plutarch.Builtin (pforgetData)
 import Plutarch.Extensions.Api (passert, pgetContinuingDatum)
 import Plutarch.Unsafe (punsafeCoerce)
@@ -71,7 +74,7 @@ paramValidator' = plam $ \countBy n r sc -> unTermCont $ do
   pmatchC r >>= \case
     Inc _ -> do
       datum <- pgetContinuingDatum @PInteger sc
-      pure $ helloLogic # countBy # n # pfromData datum
+      pure $ helloLogic # countBy # n # datum
     Spend _ -> pure $ popaque $ pcon PUnit
 
 helloLogic :: ClosedTerm (PInteger :--> PInteger :--> PInteger :--> POpaque)
