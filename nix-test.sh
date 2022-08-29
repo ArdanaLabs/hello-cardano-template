905869bd3bc86edb7e0a85340e1257b81f017c58
Evan Piro <evan.piro@platonic.systems>
2022-08-29 11:05:23 -0400

    removed extraneous files

--- nix-test.sh
+++
@@ -1,4 +0,0 @@
-nix run .#offchain:hello-world-cli:test \
-    && nix run .#offchain:hello-world-api:test \
-    && nix run .#offchain:hello-world-browser:test \
-    && nix run .#offchain:hello-world-browser:serve 
