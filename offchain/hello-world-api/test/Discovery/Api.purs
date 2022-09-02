314cd68c4ecc907f4b48a7c7ea0e5094fece9d74
Geometer1729 <16kuhnb@gmail.com>
2022-09-02 11:27:44 -0400

    add more tests

--- offchain/hello-world-api/test/Discovery/Api.purs
+++ offchain/hello-world-api/test/Discovery/Api.purs
@@ -4,19 +4,25 @@ module Test.HelloWorld.Discovery.Api
 
 import Contract.Prelude
 
+import Contract.Address (getWalletAddress)
+import Contract.Log (logInfo')
 import Contract.Monad (Contract, ContractEnv(..), liftContractAffM, liftContractM)
 import Contract.ScriptLookups as Lookups
 import Contract.Test.Plutip (PlutipConfig, withPlutipContractEnv, runContractInEnv)
+import Contract.Transaction (TransactionInput(..))
 import Contract.TxConstraints (TxConstraints)
 import Contract.TxConstraints as Constraints
+import Contract.Utxos (getUtxo, getWalletBalance)
 import Contract.Value (adaToken, scriptCurrencySymbol)
 import Contract.Wallet (withKeyWallet)
 import Data.BigInt as BigInt
+import Data.Time.Duration (Minutes(..))
 import Data.UInt as UInt
-import HelloWorld.Discovery.Api (mintNft, seedTx, makeNftPolicy)
+import Effect.Exception (throw)
+import HelloWorld.Discovery.Api (makeNftPolicy, mintNft, seedTx)
 import Plutus.Types.Value as Value
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (expectError)
+import Test.Spec.Assertions (expectError, shouldEqual)
 import Types.PlutusData (PlutusData)
 import Util (buildBalanceSignAndSubmitTx, waitForTx, withOurLogger)
 
@@ -59,6 +65,9 @@ spec = do
     describe "nft" do
       tryMintNft
       tryDoubleMint
+      txSpent
+      mintingWorks
+      cantBurn
 
 withApiLogger :: ContractEnv () -> ContractEnv ()
 withApiLogger = withOurLogger "apiTest.log"
@@ -75,13 +84,39 @@ tryMintNft = do
 
 tryDoubleMint :: Spec Unit
 tryDoubleMint = do
-  it "double minting an nft should not succeed" $ expectError do
+  it "double minting an nft should not succeed" $ do
     let initialAdaAmount = BigInt.fromInt 20_000_000
     withPlutipContractEnv config [ initialAdaAmount ] \env alice -> do
       runContractInEnv (withApiLogger env) $
         withKeyWallet alice do
-          _ <- doubleMint
-          pure unit
+          doubleMint
+
+txSpent :: Spec Unit
+txSpent = do
+  it "seedTx is spent after mint" $ do
+    let initialAdaAmount = BigInt.fromInt 20_000_000
+    withPlutipContractEnv config [ initialAdaAmount ] \env alice -> do
+      runContractInEnv (withApiLogger env) $
+        withKeyWallet alice do
+          checkTxSpent
+
+mintingWorks :: Spec Unit
+mintingWorks = do
+  it "wallet has nft after mint" $ do
+    let initialAdaAmount = BigInt.fromInt 20_000_000
+    withPlutipContractEnv config [ initialAdaAmount ] \env alice -> do
+      runContractInEnv (withApiLogger env) $
+        withKeyWallet alice do
+          mintingMints
+
+cantBurn :: Spec Unit
+cantBurn = do
+  it "burn fails after mint" $ do
+    let initialAdaAmount = BigInt.fromInt 20_000_000
+    withPlutipContractEnv config [ initialAdaAmount ] \env alice -> do
+      runContractInEnv (withApiLogger env) $
+        withKeyWallet alice do
+          burnFails
 
 -- should fail
 doubleMint :: Contract () Unit
@@ -97,6 +132,63 @@ doubleMint = do
     constraints =
       Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
   _ <- buildBalanceSignAndSubmitTx lookups constraints
-  _ <- buildBalanceSignAndSubmitTx lookups constraints
-  pure unit
+  expectError $ buildBalanceSignAndSubmitTx lookups constraints
 
+checkTxSpent :: Contract () Unit
+checkTxSpent = do
+  txOut <- seedTx
+  nftPolicy <- makeNftPolicy txOut
+  cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
+  logInfo' $ "NFT cs: " <> show cs
+  let
+    lookups :: Lookups.ScriptLookups PlutusData
+    lookups = Lookups.mintingPolicy nftPolicy
+
+    constraints :: TxConstraints Unit Unit
+    constraints =
+      Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
+  txId <- buildBalanceSignAndSubmitTx lookups constraints
+  adr <- liftContractM "no Address" =<< getWalletAddress
+  _ <- liftContractM "wait timed out" =<< waitForTx waitTime adr txId
+  getUtxo txOut >>= case _ of
+    Nothing -> pure unit
+    Just _ -> liftEffect $ throw "seed tx still existed"
+
+mintingMints :: Contract () Unit
+mintingMints = do
+  cs /\ txid <- mintNft
+  adr <- liftContractM "no wallet" =<< getWalletAddress
+  _ <- waitForTx waitTime adr txid
+  bal <- liftContractM "no ballance" =<< getWalletBalance
+  let nfts = Value.valueOf bal cs adaToken
+  nfts `shouldEqual` (BigInt.fromInt 1)
+
+burnFails :: Contract () Unit
+burnFails = do
+  txOut <- seedTx
+  nftPolicy <- makeNftPolicy txOut
+  cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
+  logInfo' $ "NFT cs: " <> show cs
+  let
+    mintLookups :: Lookups.ScriptLookups PlutusData
+    mintLookups = Lookups.mintingPolicy nftPolicy
+
+    mintConstraints :: TxConstraints Unit Unit
+    mintConstraints =
+      Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
+  txid <- buildBalanceSignAndSubmitTx mintLookups mintConstraints
+  adr <- liftContractM "no wallet" =<< getWalletAddress
+  _ <- waitForTx waitTime adr txid
+  let
+    burnLookups :: Lookups.ScriptLookups PlutusData
+    burnLookups = Lookups.mintingPolicy nftPolicy
+
+    burnConstraints :: TxConstraints Unit Unit
+    burnConstraints =
+      Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt $ -1))
+  expectError $
+    void $ buildBalanceSignAndSubmitTx burnLookups burnConstraints
+
+
+waitTime :: Minutes
+waitTime = 5.0 # Minutes
