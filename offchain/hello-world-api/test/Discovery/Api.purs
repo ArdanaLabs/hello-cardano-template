1e74043e59b35758116961e2b403bc6c24fc4180
Geometer1729 <16kuhnb@gmail.com>
2022-09-09 10:39:02 -0400

    inline specs as marijan requested

--- offchain/hello-world-api/test/Discovery/Api.purs
+++ offchain/hello-world-api/test/Discovery/Api.purs
@@ -4,6 +4,7 @@ module Test.HelloWorld.Discovery.Api
 
 import Contract.Prelude
 
+
 import Contract.ScriptLookups as Lookups
 import Contract.TxConstraints as Constraints
 import Data.BigInt as BigInt
@@ -20,23 +21,90 @@ import Contract.Wallet (withKeyWallet)
 import Data.Time.Duration (Minutes(..))
 import Effect.Exception (throw)
 import HelloWorld.Discovery.Api (makeNftPolicy, mintNft, seedTx)
-import Test.HelloWorld.EnvRunner (EnvRunner)
-import Test.Spec (Spec, describe, it)
+import Test.HelloWorld.EnvRunner (EnvRunner,runEnvSpec)
+import Test.Spec (Spec ,describe, it)
 import Test.Spec.Assertions (expectError, shouldEqual)
 import Types.PlutusData (PlutusData)
 import Util (buildBalanceSignAndSubmitTx, waitForTx, withOurLogger, maxWait)
 
 spec :: EnvRunner -> Spec Unit
-spec runner = do
-  describe "HelloWorld.Discovery.Api" $ do
+spec = runEnvSpec do
+  describe "HelloWorld.Discovery.Api" do
     describe "nft" do
-      traverse_ (_ $ runner)
-        [ tryMintNft
-        , tryDoubleMint
-        , txSpentAfterMint
-        , mintingWorks
-        , cantBurn
-        ]
+
+      it "mint runs" $ useRunnerSimple do
+        mintNft
+
+      it "double minting fails on second mint" $ useRunnerSimple do
+        txOut <- seedTx
+        nftPolicy <- makeNftPolicy txOut
+        cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
+        let
+          lookups :: Lookups.ScriptLookups PlutusData
+          lookups = Lookups.mintingPolicy nftPolicy
+
+          constraints :: TxConstraints Unit Unit
+          constraints =
+            Constraints.mustSpendPubKeyOutput txOut
+              <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
+        txId <- buildBalanceSignAndSubmitTx lookups constraints
+        adr <- liftContractM "no wallet" =<< getWalletAddress
+        _ <- waitForTx maxWait adr txId
+        expectError $ buildBalanceSignAndSubmitTx lookups constraints
+
+      it "seedTx is spent after mint" $ useRunnerSimple do
+        txOut <- seedTx
+        nftPolicy <- makeNftPolicy txOut
+        cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
+        logInfo' $ "NFT cs: " <> show cs
+        let
+          lookups :: Lookups.ScriptLookups PlutusData
+          lookups = Lookups.mintingPolicy nftPolicy
+
+          constraints :: TxConstraints Unit Unit
+          constraints =
+            Constraints.mustSpendPubKeyOutput txOut
+              <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
+        txId <- buildBalanceSignAndSubmitTx lookups constraints
+        adr <- liftContractM "no Address" =<< getWalletAddress
+        _ <- liftContractM "wait timed out" =<< waitForTx waitTime adr txId
+        getUtxo txOut >>= case _ of
+          Nothing -> pure unit
+          Just _ -> liftEffect $ throw "seed tx still existed"
+
+      it "wallet has nft after mint" $ useRunnerSimple do
+        cs <- mintNft
+        bal <- liftContractM "no ballance" =<< getWalletBalance
+        let nfts = Value.valueOf bal cs adaToken
+        nfts `shouldEqual` (BigInt.fromInt 1)
+
+      it "burning nft fails" $ useRunnerSimple do
+        txOut <- seedTx
+        nftPolicy <- makeNftPolicy txOut
+        cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
+        logInfo' $ "NFT cs: " <> show cs
+        let
+          mintLookups :: Lookups.ScriptLookups PlutusData
+          mintLookups = Lookups.mintingPolicy nftPolicy
+
+          mintConstraints :: TxConstraints Unit Unit
+          mintConstraints =
+            Constraints.mustSpendPubKeyOutput txOut
+              <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
+        txid <- buildBalanceSignAndSubmitTx mintLookups mintConstraints
+        adr <- liftContractM "no wallet" =<< getWalletAddress
+        _ <- waitForTx waitTime adr txid
+        let
+          burnLookups :: Lookups.ScriptLookups PlutusData
+          burnLookups = Lookups.mintingPolicy nftPolicy
+
+          burnConstraints :: TxConstraints Unit Unit
+          burnConstraints =
+            Constraints.mustSpendPubKeyOutput txOut
+              <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt $ -1))
+        expectError
+          $ void
+          $ buildBalanceSignAndSubmitTx burnLookups burnConstraints
 
 useRunnerSimple :: forall a. Contract () a -> EnvRunner -> Aff Unit
 useRunnerSimple contract runner = do
@@ -44,86 +112,6 @@ useRunnerSimple contract runner = do
     runContractInEnv (withOurLogger "apiTest.log" env)
       $ withKeyWallet alice
       $ void contract
-
-tryMintNft :: EnvRunner -> Spec Unit
-tryMintNft = it "mint runs" <$> useRunnerSimple mintNft
-
-tryDoubleMint :: EnvRunner -> Spec Unit
-tryDoubleMint =
-  it "double minting fails on second mint" <$> useRunnerSimple do
-    txOut <- seedTx
-    nftPolicy <- makeNftPolicy txOut
-    cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
-    let
-      lookups :: Lookups.ScriptLookups PlutusData
-      lookups = Lookups.mintingPolicy nftPolicy
-
-      constraints :: TxConstraints Unit Unit
-      constraints =
-        Constraints.mustSpendPubKeyOutput txOut
-          <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
-    txId <- buildBalanceSignAndSubmitTx lookups constraints
-    adr <- liftContractM "no wallet" =<< getWalletAddress
-    _ <- waitForTx maxWait adr txId
-    expectError $ buildBalanceSignAndSubmitTx lookups constraints
-    pure unit
-
-txSpentAfterMint :: EnvRunner -> Spec Unit
-txSpentAfterMint = it "seedTx is spent after mint" <$> useRunnerSimple do
-  txOut <- seedTx
-  nftPolicy <- makeNftPolicy txOut
-  cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
-  logInfo' $ "NFT cs: " <> show cs
-  let
-    lookups :: Lookups.ScriptLookups PlutusData
-    lookups = Lookups.mintingPolicy nftPolicy
-
-    constraints :: TxConstraints Unit Unit
-    constraints =
-      Constraints.mustSpendPubKeyOutput txOut
-        <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
-  txId <- buildBalanceSignAndSubmitTx lookups constraints
-  adr <- liftContractM "no Address" =<< getWalletAddress
-  _ <- liftContractM "wait timed out" =<< waitForTx waitTime adr txId
-  getUtxo txOut >>= case _ of
-    Nothing -> pure unit
-    Just _ -> liftEffect $ throw "seed tx still existed"
-
-mintingWorks :: EnvRunner -> Spec Unit
-mintingWorks = it "wallet has nft after mint" <$> useRunnerSimple do
-  cs <- mintNft
-  bal <- liftContractM "no ballance" =<< getWalletBalance
-  let nfts = Value.valueOf bal cs adaToken
-  nfts `shouldEqual` (BigInt.fromInt 1)
-
-cantBurn :: EnvRunner -> Spec Unit
-cantBurn = it "burning nft fails" <$> useRunnerSimple do
-  txOut <- seedTx
-  nftPolicy <- makeNftPolicy txOut
-  cs <- liftContractAffM "hash failed" $ scriptCurrencySymbol nftPolicy
-  logInfo' $ "NFT cs: " <> show cs
-  let
-    mintLookups :: Lookups.ScriptLookups PlutusData
-    mintLookups = Lookups.mintingPolicy nftPolicy
-
-    mintConstraints :: TxConstraints Unit Unit
-    mintConstraints =
-      Constraints.mustSpendPubKeyOutput txOut
-        <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt 1))
-  txid <- buildBalanceSignAndSubmitTx mintLookups mintConstraints
-  adr <- liftContractM "no wallet" =<< getWalletAddress
-  _ <- waitForTx waitTime adr txid
-  let
-    burnLookups :: Lookups.ScriptLookups PlutusData
-    burnLookups = Lookups.mintingPolicy nftPolicy
-
-    burnConstraints :: TxConstraints Unit Unit
-    burnConstraints =
-      Constraints.mustSpendPubKeyOutput txOut
-        <> Constraints.mustMintValue (Value.singleton cs adaToken (BigInt.fromInt $ -1))
-  expectError
-    $ void
-    $ buildBalanceSignAndSubmitTx burnLookups burnConstraints
 
 waitTime :: Minutes
 waitTime = 5.0 # Minutes
