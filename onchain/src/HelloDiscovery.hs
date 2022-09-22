c551eec6f7c56f76f6e8e1d2580ada88ae907352
Geometer1729 <16kuhnb@gmail.com>
2022-09-22 17:40:02 -0400

    make the onchain code a bit more restrictive

--- onchain/src/HelloDiscovery.hs
+++ onchain/src/HelloDiscovery.hs
@@ -38,7 +38,7 @@ import PlutusLedgerApi.V2 (MintingPolicy, TxOutRef, adaToken)
 import Plutarch.Api.V1.AssocMap qualified as AssocMap
 import Plutarch.Api.V1.Value qualified as Value
 
-import Plutarch.Api.V1.Value (pforgetPositive)
+import Plutarch.Api.V1.Value (pvalueOf)
 import Plutarch.Builtin (pforgetData, pserialiseData)
 import Plutarch.Crypto (pblake2b_256)
 import Plutarch.DataRepr (PDataFields)
@@ -46,6 +46,7 @@ import Plutarch.Extensions.Api (passert, passert_, pfindOwnInput, pgetContinuing
 import Plutarch.Extensions.Data (parseData, parseDatum)
 import Plutarch.Extensions.List (unsingleton)
 import Plutarch.Extra.TermCont
+import Plutarch.Extensions.Monad (pmatchFieldC)
 
 configScriptCbor :: String
 configScriptCbor = validatorToHexString $ mkValidator globalConfig configScript
@@ -117,11 +118,10 @@ authTokenMP = ptrace "vault auth mp" $
     plam $ \vaultAdrData redeemerData sc -> unTermCont $ do
       (redeemer :: Term _ AuthRedeemer) <- parseData redeemerData
       info <- pletC $ pfield @"txInfo" # sc
-      let (minting' :: Term _ (PValue _ _)) = pfield @"mint" # info
+      let (minting :: Term _ (PValue _ _)) = pfield @"mint" # info
       PMinting cs' <- pmatchC (pfield @"purpose" # sc)
       cs :: Term _ PCurrencySymbol <- pletC $ pfield @"_0" # cs'
-      PValue minting <- pmatchC minting'
-      PJust mintedAtCs <- pmatchC $ AssocMap.plookup # cs # minting
+      mintedAtCs <- pletC $ atCS # minting # cs
       pmatchC redeemer >>= \case
         Burning _ -> do
           passert "was just burning" $
@@ -143,7 +143,7 @@ authTokenMP = ptrace "vault auth mp" $
 
           -- mints exactly one token
           passert_ "did not mint exactly one token of this currency symbol" $
-            mintedAtCs #== (AssocMap.psingleton # tn # 1)
+            isJustTn # mintedAtCs # tn
 
           -- Exactly one vault output
           let outputs = pfield @"outputs" # info
@@ -152,7 +152,7 @@ authTokenMP = ptrace "vault auth mp" $
 
           -- NFT sent to vault
           val <- pletC $ pfield @"value" # vault
-          passert_ "nft went to vault" $ (Value.psingleton # cs # tn # 1) #<= pforgetPositive val
+          passert_ "nft went to vault" $ pvalueOf # val # cs # tn #== 1
 
           -- Counter starts at 0
           outDatum <- pletC $ pfield @"datum" # vault
@@ -190,17 +190,14 @@ vaultAdrValidator = ptrace "vaultAdrValidator" $
     redeemer :: Term _ HelloRedeemer <- parseData redeemer'
     configNftCs :: Term _ PCurrencySymbol <- parseData configNftCsData
     PJust config <- pmatchC $ pfind # (isConfigInput # configNftCs) # (pfield @"referenceInputs" # info)
-    configDatum <-
-      pmatchC (pfield @"datum" #$ pfield @"resolved" # config) >>= \case
-        POutputDatum d -> pure $ pfield @"outputDatum" # d
-        POutputDatumHash _ -> fail "datum hash not supported"
-        PNoOutputDatum _ -> fail "no data"
-    PDatum configData <- pmatchC configDatum
+    POutputDatum configDatum <- pmatchC (pfield @"datum" #$ pfield @"resolved" # config)
+    PDatum configData <- pmatchFieldC @"outputDatum"  configDatum
     nftCs <- parseData configData
     PSpending outRef <- pmatchC $ pfield @"purpose" # sc
+    -- TODO enforce that this is also the only input of that address
     PJust inInfo <- pmatchC $ pfindOwnInput # (pfield @"inputs" # info) #$ pfield @"_0" # outRef
     nftTn :: Term _ PTokenName <- pletC $ pfield @"tokenName" # redeemer
-    passert_ "has nft" $ 0 #< (Value.pvalueOf # (pfield @"value" #$ pfield @"resolved" # inInfo) # nftCs # nftTn)
+    passert_ "has nft" $ hasExactlyThisNft # nftCs # nftTn # (pfield @"value" #$ pfield @"resolved" # inInfo)
     pmatchC (pfield @"action" # redeemer) >>= \case
       Inc _ -> do
         out <- pgetContinuingOutput sc
@@ -208,9 +205,7 @@ vaultAdrValidator = ptrace "vaultAdrValidator" $
         datum2 :: Term _ CounterDatum <- parseDatum (pfield @"outputDatum" # outDatum)
         passert_ "owner is the same" $ pfield @"owner" # datum2 #== pfield @"owner" # datum
         passert_ "count is 1 more" $ pfield @"count" # datum2 #== pfield @"count" # datum + (1 :: Term _ PInteger)
-        PValue outVal <- pmatchC $ pfield @"value" # out
-        PJust (outputNfts :: Term _ (PMap _ PTokenName PInteger)) <- pmatchC $ AssocMap.plookup # nftCs # outVal
-        passert "kept nft" $ outputNfts #== AssocMap.psingleton # nftTn # 1
+        passert "kept nft" $ hasExactlyThisNft # nftCs # nftTn # (pfield @"value" #out)
       Spend _ -> do
         let minting = pfield @"mint" # info
         passert "burned nft" $ Value.pvalueOf # minting # nftCs # nftTn #< 0
@@ -223,6 +218,19 @@ isConfigInput = phoistAcyclic $
     let isAtConfigAdr = (pfield @"credential" #$ pfield @"address" # out) #== configAdr
     let hasConfigNft = 0 #< Value.pvalueOf # (pfield @"value" # out) # cs # pconstant adaToken
     pure $ isAtConfigAdr #&& hasConfigNft
+
+hasExactlyThisNft :: ClosedTerm (PCurrencySymbol :--> PTokenName :--> PValue 'Value.Sorted a :--> PBool)
+hasExactlyThisNft = phoistAcyclic $ plam $ \cs tn val -> isJustTn # (atCS # val # cs) # tn
+
+isJustTn :: ClosedTerm (PMap 'Value.Sorted PTokenName PInteger :--> PTokenName :--> PBool)
+isJustTn = phoistAcyclic $ plam $ \m tn -> m #== AssocMap.psingleton # tn # 1
+
+atCS :: ClosedTerm (PValue s a :--> PCurrencySymbol :--> PMap s PTokenName PInteger)
+atCS = phoistAcyclic $
+  plam $ \val cs -> unTermCont $ do
+    PValue valMap <- pmatchC val
+    PJust subMap <-pmatchC $ AssocMap.plookup # cs # valMap
+    pure subMap
 
 -- Types
 
