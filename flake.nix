672f3ac265c96399e7af3d5915b97d8a22e9addd
Geometer1729 <16kuhnb@gmail.com>
2022-10-05 16:45:27 -0400

    remove comment that's nolonger accurate

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cardano-node.url = "github:input-output-hk/cardano-node?rev=73f9a746362695dc2cb63ba757fbcabb81733d23";
-    # CTL with cardano-wallet fix
     cardano-transaction-lib.url = "github:Plutonomicon/cardano-transaction-lib?rev=1999bb962141ffea09767f299a5759420097d189";
     cardano-ogmios.url = "github:input-output-hk/cardano-ogmios";
     mlabs-ogmios.follows = "cardano-transaction-lib/ogmios";
