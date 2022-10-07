ec59717a992999929edde294e6cf32369626d4b5
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-10-07 18:05:52 -0500

    offchain/nixos: add hello-world module

---
+++ offchain/nixos/modules/hello-world.nix
@@ -0,0 +1,115 @@
+{ lib, pkgs, config, ... }:
+let
+  inherit (lib)
+    types
+    mkOption
+    mkIf
+    mkMerge
+    mkEnableOption
+    mdDoc
+    ;
+  cfg = config.services.hello-world;
+in
+{
+  options.services.hello-world = {
+
+    enable = mkEnableOption "enable serving the hello-world application";
+
+    package = mkOption {
+      type = types.package;
+    };
+
+    port = mkOption {
+      type = types.port;
+      default = 8080;
+      example = 8080;
+      description = mdDoc ''
+        Port to listen on.
+      '';
+    };
+
+    ctlRuntimeConfig = {
+      ogmiosConfig = {
+        host = mkOption {
+          type = types.str;
+          default = "127.0.0.0";
+          example = "ogmios-service.com";
+          description = mdDoc ''
+            Ogmios service host address.
+          '';
+        };
+        port = mkOption {
+          type = types.port;
+          default = 1337;
+          example = 1337;
+          description = mdDoc ''
+            Ogmios service host port.
+          '';
+        };
+      };
+      datumCacheConfig = {
+        host = mkOption {
+          type = types.str;
+          default = "127.0.0.0";
+          example = "ogmios-service.com";
+          description = mdDoc ''
+            Ogmios datum cache service host address.
+          '';
+        };
+        port = mkOption {
+          type = types.port;
+          default = 9999;
+          example = 9999;
+          description = mdDoc ''
+            Ogmios datum cache service host port.
+          '';
+        };
+      };
+      ctlServerConfig = {
+        host = mkOption {
+          type = types.str;
+          default = "127.0.0.0";
+          example = "ogmios-service.com";
+          description = mdDoc ''
+            CTL server host address.
+          '';
+        };
+        port = mkOption {
+          type = types.port;
+          default = 8081;
+          example = 8081;
+          description = mdDoc ''
+            CTL server host port.
+          '';
+        };
+      };
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    systemd.services.hello-world =
+      let
+        packageWithCtlRuntimeConfig = pkgs.runCommand "package-with-ctl-runtime-config" { } ''
+          mkdir -p $out/dist
+          echo '${builtins.toJSON cfg.ctlRuntimeConfig}' > $out/dist/ctl-runtime-config.json
+          cp -r ${cfg.package}/* $out/
+          ls -lisa $out/dist
+        '';
+      in
+      {
+        description = "hello-world-server";
+        documentation = [ "https://github.com/ArdanaLabs/cardano-app-template" ];
+        wantedBy = [ "multi-user.target" ];
+        after = [ "ctl-server.service" "networking.target" ];
+        serviceConfig = mkMerge [
+          {
+            ExecStart = ''${pkgs.simple-http-server}/bin/simple-http-server -c=js,css,svg,html -i -p ${toString cfg.port} -- ${packageWithCtlRuntimeConfig}'';
+            Restart = "always";
+            DynamicUser = true;
+          }
+        ];
+      };
+  };
+}
