df02c3cdab12b1c68c385610832fcad043d508e2
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-11-02 16:25:25 -0500

    offchain/nixos: add more variety in hello-world module test

--- offchain/nixos/tests/hello-world.nix
+++ offchain/nixos/tests/hello-world.nix
@@ -16,6 +16,20 @@ nixosTest {
       services.hello-world = {
         enable = true;
         port = helloWorldServerPort;
+        ctlRuntimeConfig.local = {
+          ctlServerConfig = {
+            host = "127.0.0.1";
+            port = 8081;
+          };
+          ogmiosConfig = {
+            host = "127.0.0.1";
+            port = 8082;
+          };
+          datumCacheConfig = {
+            host = "127.0.0.1";
+            port = 8083;
+          };
+        };
       };
       networking.firewall.allowedTCPPorts = [ helloWorldServerPort ];
     };
@@ -28,7 +42,7 @@ nixosTest {
       virtualisation.memorySize = 2048;
       test-support.displayManager.auto.user = "alice";
       environment = {
-        systemPackages = [ pkgs.chromium pkgs.xdotool ];
+        systemPackages = with pkgs; [ chromium xdotool curl jq ];
         variables."XAUTHORITY" = "/home/alice/.Xauthority";
       };
     };
@@ -48,6 +62,7 @@ nixosTest {
     server.wait_for_open_port(${builtins.toString helloWorldServerPort})
 
     client.wait_for_x()
+    client.succeed("curl http://server:${builtins.toString helloWorldServerPort}/dist/ctl-runtime-config.json | jq '.ctlServerConfig'")
     client.succeed("su - alice -c 'ulimit -c unlimited; chromium http://server:${builtins.toString helloWorldServerPort} >&2 & disown'")
     client.wait_for_window("Hello World")
     # focus the chromium window displaying Hello World
