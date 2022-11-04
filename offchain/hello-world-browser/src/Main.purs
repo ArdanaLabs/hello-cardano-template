37378300790ca5e61a3808c4668353e3ed58fe74
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-11-04 15:46:48 -0500

    offchain/browser: fix logic loading cookie when not using keyWallet

--- offchain/hello-world-browser/src/Main.purs
+++ offchain/hello-world-browser/src/Main.purs
@@ -75,8 +75,18 @@ main =
     -- the mainnetConfig is defined as `testnetConfig { networkId = MainnetId }` in CTL
     -- see https://github.com/Plutonomicon/cardano-transaction-lib/blob/886ac0a08989e5c4928f907f3f86448e8836c799/src/Contract/Config.purs#L86
     configParams <- getConfigParams
-    walletSpec <- loadWalletSpec
-    networkId <- loadNetworkId
+
+    -- We load the walletspec and network id
+    -- from cookies for testing.
+    -- if we don't use the keyWallet we
+    -- assume we are in production
+    walletSpec <- useKeyWallet >>= case _ of
+      true -> loadWalletSpecFromCookie
+      false -> pure ConnectToNami
+
+    networkId <- useKeyWallet >>= case _ of
+      true -> loadNetworkIdFromCookie
+      false -> pure MainnetId
 
     let
       contractConfig = configParams
@@ -98,29 +108,26 @@ useKeyWallet :: Aff Boolean
 useKeyWallet =
   isJust <$> getCookie "paymentKey"
 
-loadWalletSpec :: Aff WalletSpec
-loadWalletSpec = do
-  useKeyWallet >>= case _ of
-    false -> pure ConnectToNami
-    true -> do
-      mPaymentKeyStr <- getCookie "paymentKey"
-      paymentKeyStr <- liftM (error "payment key not found in the cookie") mPaymentKeyStr
-      paymentKeyBytes <- liftEither $ lmap (error <<< printTextEnvelopeDecodeError) $
-        textEnvelopeBytes paymentKeyStr PaymentSigningKeyShelleyed25519
-      paymentKey <- liftM (error "Unable to decode private payment key")
-        $ PrivatePaymentKey
-        <$> privateKeyFromBytes (wrap paymentKeyBytes)
+loadWalletSpecFromCookie :: Aff WalletSpec
+loadWalletSpecFromCookie = do
+  mPaymentKeyStr <- getCookie "paymentKey"
+  paymentKeyStr <- liftM (error "payment key not found in the cookie") mPaymentKeyStr
+  paymentKeyBytes <- liftEither $ lmap (error <<< printTextEnvelopeDecodeError) $
+    textEnvelopeBytes paymentKeyStr PaymentSigningKeyShelleyed25519
+  paymentKey <- liftM (error "Unable to decode private payment key")
+    $ PrivatePaymentKey
+    <$> privateKeyFromBytes (wrap paymentKeyBytes)
 
-      getCookie "stakeKey" >>= case _ of
-        Nothing -> pure $ UseKeys (PrivatePaymentKeyValue paymentKey) Nothing
-        Just stakeKeyStr -> do
-          stakeKeyBytes <- liftEither $ lmap (error <<< printTextEnvelopeDecodeError) $
-            textEnvelopeBytes stakeKeyStr StakeSigningKeyShelleyed25519
-          let stakeKey = PrivateStakeKey <$> privateKeyFromBytes (wrap stakeKeyBytes)
-          pure $ UseKeys (PrivatePaymentKeyValue paymentKey) (PrivateStakeKeyValue <$> stakeKey)
+  getCookie "stakeKey" >>= case _ of
+    Nothing -> pure $ UseKeys (PrivatePaymentKeyValue paymentKey) Nothing
+    Just stakeKeyStr -> do
+      stakeKeyBytes <- liftEither $ lmap (error <<< printTextEnvelopeDecodeError) $
+        textEnvelopeBytes stakeKeyStr StakeSigningKeyShelleyed25519
+      let stakeKey = PrivateStakeKey <$> privateKeyFromBytes (wrap stakeKeyBytes)
+      pure $ UseKeys (PrivatePaymentKeyValue paymentKey) (PrivateStakeKeyValue <$> stakeKey)
 
-loadNetworkId :: Aff NetworkId
-loadNetworkId = do
+loadNetworkIdFromCookie :: Aff NetworkId
+loadNetworkIdFromCookie = do
   mNetworkId <- getCookie "networkId"
   networkId <- liftM (error "network ID not found in the cookie") mNetworkId
   case networkId of
